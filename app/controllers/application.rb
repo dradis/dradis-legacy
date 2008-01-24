f4b84e4cf67e72a432857ac2bcd6bec2ea3f9ab6
Daniel Martin <etd(at)nomejortu.com>
2008-01-24 01:22:12 +0000

    application.rb: implement HTTP Basic authentication as some layer of security. The idea is that testers of the team agree on a password and then each of them logs in using their username and the common password. The username is stored in the session and it may be used in the future to auto-fill the 'author' field in the notes added by that user.
    dradis.css: only modify the css attributes of those html form elements under
    the #summary div. Remove the strange background color of #wrap that only showed
    on IE.
    
    main.html: rearrange the right hand menu items to clearly split what's active
    scaffolding from what's dradis specific.
    
    summary_helper.rb: two new functions added to centralize the creation of
    'action links'. Action links are those that html <a> tags that affect the Notes
    (edit/del/details/back). It is best to have them in a single well structured
    place rather than in the different partials were they are required.
    
    summary_controller.rb: new action 'update' for inline editing of the notes.
    
      -Other actions have been modified to standarize the behaviour, mostly
      regarding the passing of global variables such as the @parent_type, @parent_id
      and @position ones.
      
      - 'notes' action has been modified and does not render a partial anymore. It
      now uses the RJS functionality to update both the <div> element that displays
      the notes, and the <span> element that shows the number of notes associated
      with a given parent.
    
    /app/views/summary: lots of change.
      * The index.rhtml displays two <divs>, the hosts' <div> (on the left hand)
      and the notes' <div> (right). The hosts' is populated straight away, the
      notes' is populated through the _notes partial. The @position variable is
      used to keep track of the hosts' <div> associated with the active note.
      
      * _notes.rhtml: creates the 'add note' form plus a list of notes (by calling
      the _note partial for each note of the associated Annotatable).
      
      * _note.rhtml: is a dummy partial that redirects the flow to the correct view
      associated with the note in each moment. Notes can either be in
      short/full/edit mode.
      
      * _note_short.rhtml: Displays a condensed version of the note. If the text is
      too long, it's cropped.
      
      * _note_full.rhtml: Displays a full view of the note's text.
      
      * _note_edit.rhtml: Useful to modify the details of a given note

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,4 +5,51 @@ class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_dserver_session_id'
   layout 'main'
+  before_filter :authorise
+
+
+  private
+    def authorise
+      # Log in using session data if it exists.
+      if session['user']
+        p session['user']
+        @user = session['user'] and return true
+      end
+
+      # Otherwise log in from authentication data sent in request header.
+      login, password = get_auth_data
+      if login and password and password == 'etd001'
+        session["user"] = login
+        @user = login
+        return true
+      end
+
+      # If no auth data, or wrong auth data, issue a challenge.
+      response.headers["Status"] = "Unauthorized" 
+      response.headers["WWW-Authenticate"] = 'Basic realm="st. eves"'
+      render(:text => "Authentication required", :status => 401)       
+    end 
+
+    def deauthorise
+      session.delete("account_id")
+    end
+
+    def get_auth_data 
+      auth_data = nil
+      [
+        'REDIRECT_REDIRECT_X_HTTP_AUTHORIZATION',
+        'REDIRECT_X_HTTP_AUTHORIZATION',
+        'X-HTTP_AUTHORIZATION', 
+        'HTTP_AUTHORIZATION'
+      ].each do |key|
+        if request.env.has_key?(key)
+          auth_data = request.env[key].to_s.split
+          break
+        end
+      end
+
+      if auth_data && auth_data[0] == 'Basic' 
+        return Base64.decode64(auth_data[1]).split(':')[0..1] 
+      end 
+    end 
 end
--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -1,2 +1,44 @@
 module SummaryHelper
+  def link_for_action(title, action, opt={}, update_note=false, confirm=false)
+    default = { 
+	       :action => action, 
+	       :id => @note.id, 
+	       :parent_type => @parent_type, 
+	       :parent_id => @parent_id, 
+	       :position => @position
+	      }
+
+    if update_note
+      if confirm
+        link_to_remote( title, { :url => default.merge(opt), :update => "note_#{@note.id}", :confirm => 'Sure?' } )
+      else
+        link_to_remote( title, { :url => default.merge(opt), :update => "note_#{@note.id}" } )
+      end
+    else
+      if confirm
+        link_to_remote( title, { :url => default.merge(opt), :confirm => 'Sure?'} )
+      else
+        link_to_remote( title, :url => default.merge(opt) )
+      end
+    end
+  end
+
+  def link_bar
+    links = [link_for_action( 'remove', :del, {}, false, true )]
+    
+    if @view == 'edit'
+      links.unshift( link_for_action( 'back', :show, {:view => @previous}, true ) )
+    else
+      links.unshift( link_for_action( 'edit', :show, {:view => 'edit', :previous => @view }, true ) )
+      if @view == 'full'
+        links.unshift( link_for_action( 'back', :show, {:view => 'short'}, true ) )
+      else
+        if @cropped
+          links.unshift( link_for_action( 'details', :show, { :view => 'full'}, true ) )
+        end
+      end
+    end
+
+    links.collect do |l| " [#{l}]" end.join
+  end
 end
