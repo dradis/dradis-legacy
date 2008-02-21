524f064717116a3015cd3d9f3984048031dfca79
Daniel Martin <etd(at)nomejortu.com>
2008-02-21 20:25:38 +0000

    application.rb: disable session support since is not needed at the moment and produced a crash in the reserver due to too many open files.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,8 @@
 
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_dserver_session_id'
+  #session :session_key => '_dserver_session_id'
+  session :disabled => true
   layout 'main'
   # not ye implemented, plus it breaks the client web service calls :)
   #before_filter :authorise
