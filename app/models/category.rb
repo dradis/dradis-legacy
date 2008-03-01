24096ac40b944eebf81b584f68c0ec4fbb436ef9
Daniel Martin <etd(at)nomejortu.com>
2008-03-01 04:08:12 +0000

    - add uniqueness validation to both categories and protocols. Each needs to have a name, and it must be unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,3 +1,4 @@
 class Protocol < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
