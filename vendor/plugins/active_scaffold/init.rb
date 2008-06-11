3965a1f649a4620042cc48e590058cdcb13b727d
Daniel Martin <etd(at)nomejortu.com>
2008-06-11 16:49:57 +0000

    Fix commit for Windows Vista. Because Vista doesn't like the apps writting in "Program Files" directory, we need to ensure that dradis server doesn't do this. First thing, ammed active_scaffold init process. The original process copied the plugin files to the RAILS_ROOT. This is only needed the first time, no need to do it again and again.

--- vendor/plugins/active_scaffold/init.rb
+++ vendor/plugins/active_scaffold/init.rb
@@ -6,4 +6,6 @@ require File.dirname(__FILE__) + '/environment'
 ##
 ## Run the install script, too, just to make sure
 ##
-require File.dirname(__FILE__) + '/install'
+## there is no need to repeat this step again and again. besides, it kills
+## Vista install (we are not allowed to copy inside Program Files)
+## require File.dirname(__FILE__) + '/install'
