9cee3ce238ce839aeadf8e403b0e96044f432b67
Daniel Martin <etd(at)nomejortu.com>
2008-09-28 17:37:58 +0000

    Reimplement .to_json() so it returns a JSON object that is understood by the TreeLoader of the ExtJS library

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,17 @@
 class Node < ActiveRecord::Base
   acts_as_tree
   has_many :notes
+
+  def to_json(options={})
+    json = '{"text":"'
+    json << self.label
+    json << '"'
+    if (self.children.any?)
+      json << ', "children":'
+      json << self.children.to_json
+    else
+      json << ',"leaf":true'
+    end
+    json << '}'
+  end
 end
