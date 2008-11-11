0aeee1f4a91b10eb6f2e31c680fbd12de8dda8be
Daniel Martin <etd(at)nomejortu.com>
2008-11-11 00:40:49 +0000

    Add a note method to create note from the Ext-JS interface

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -5,4 +5,16 @@ class JsonController < ApplicationController
     nodes = Node.find(:all, :conditions => {:parent_id => nil})
     render :text => nodes.to_json
   end
+
+  def note  
+    # TODO: validation!!
+    Note.new( 
+      :text => params[:text],
+      :author => params[:author],
+      :node_id => params[:node],
+      :category_id => params[:category] 
+    ).save
+    render :text => 'noerror'
+  end
+
 end
