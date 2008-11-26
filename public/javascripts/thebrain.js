35a0a13a79d9c6a9c23eb47e966a524885aef5f2
Daniel Martin <etd(at)nomejortu.com>
2008-11-26 15:58:47 +0000

    New JavaScript file that contains methods that do the Ajax calls. This has to be evolved into an object that does all the Ajax stuff in a centralised way. For the time being, a functional approach has been taken

---
+++ public/javascripts/thebrain.js
@@ -0,0 +1,34 @@
+function addnode(node, callback) {
+  var parent = node.parentNode;
+  console.log( 'thebrain: addnode' );
+  console.log( node.id );
+  console.log( node.text );
+  console.log( parent.id );
+  var p = { 
+    label: node.text, 
+  }
+  if (parent.id != 'root-node') {
+    p.parent_id = parent.id
+  }
+  p.authenticity_token = dradis.token;
+  Ext.Ajax.request({
+    url: '/json/node_create',
+    params: p, 
+    success: function(response, options) {
+                   dradisstatus.setStatus({ 
+                        text: 'New node sent to the server',
+                        clear: 5000
+                   });
+              callback(response.responseText);
+    },
+    failure: function(response, options) {
+                   dradisstatus.setStatus({
+                        text: 'An error occured with the Ajax request',
+                        iconCls: 'error',
+                        clear: 5000
+                   });
+    },
+  });
+
+  console.log( 'thebrain: /addnode' );
+}
