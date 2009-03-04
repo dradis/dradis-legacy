632eae7120a8c16407fbee6927c83c48bdf40d8b
Daniel Martin <etd(at)nomejortu.com>
2009-03-04 20:53:00 +0000

    Structure the code of the plugin:   processor.rb: contains the brain power to generate the Word XML document. It                 will be used by the web interface and the Rake task   actions.rb: will define the actions to be incorporated to the ExportContoller

---
+++ vendor/plugins/word_export/lib/word_export/processor.rb
@@ -0,0 +1,11 @@
+module WordExport
+  # The Processor class will walk the repository and extract the information 
+  # and generate a Word XML file using a template.
+  class Processor
+    private
+    public
+    def generate()
+      
+    end
+  end
+end
