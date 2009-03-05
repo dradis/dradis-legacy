72e3b6e726b903810ad295a3d79140125fb6df5a
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-03-05 20:55:25 +0000

    Check to see if db access is possible - code breaks at rake db:migrate if db connection is not checked

--- vendor/plugins/word_export/init.rb
+++ vendor/plugins/word_export/init.rb
@@ -1,6 +1,6 @@
 # Include hook code here
 require 'word_export'
 
-if (Category.find_by_name(WordExport::REPORTING_CATEGORY_NAME).nil?)
+if (Category.connected? && Category.find_by_name(WordExport::REPORTING_CATEGORY_NAME).nil?)
   Category.new(:name => WordExport::REPORTING_CATEGORY_NAME).save
 end
