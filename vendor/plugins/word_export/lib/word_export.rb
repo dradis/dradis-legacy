9c55a2630b667fa64cd76d189f5beda34bdf67d5
Daniel Martin <etd(at)nomejortu.com>
2009-03-05 15:21:43 +0000

    The plugin will only try to process those notes associated with a specific category. This category is created by the plugin if it doesn't exist in the DB and its name is given by the WordExport::REPORTING_CATEGORY_NAME constant defined in lib/word_export.rb.

--- vendor/plugins/word_export/lib/word_export.rb
+++ vendor/plugins/word_export/lib/word_export.rb
@@ -3,6 +3,10 @@
 require 'word_export/processor'
 require 'word_export/actions'
 
+module WordExport
+  REPORTING_CATEGORY_NAME = 'WordExport ready'
+end
+
 module Plugins
   module Export
     include WordExport::Actions
