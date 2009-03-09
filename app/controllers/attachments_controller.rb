afbeb595a141ce1a058fde8eda2c5eb1802154cc
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-03-09 23:23:28 +0000

    View and controller method to handle attachment upload

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,4 +3,12 @@ class AttachmentsController < ApplicationController
   def index
     @attachments = Node.find(params[:node_id]).attachments
   end
+
+  def create
+    @attachment = Attachment.new(params['attachment_file'].original_filename, :node_id => params[:node_id])
+    @attachment << params['attachment_file'].read
+    @attachment.save
+    debugger
+    redirect_to node_attachments_path(params[:node_id])
+  end
 end
