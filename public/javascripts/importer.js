f2375f1b69eeab5e5dcef3c25c99b7ca661f2ae3
Daniel Martin <etd(at)nomejortu.com>
2009-03-09 16:06:42 +0000

    Quick scaffold of the +Import from...+ tab.

--- public/javascripts/importer.js
+++ public/javascripts/importer.js
@@ -3,17 +3,46 @@ Ext.ns('dradis');
 dradis.NotesImporter = Ext.extend(Ext.Panel, {
     // Prototype Defaults, can be overridden by user's config object
     title: 'Import from...',
+    layout:'form',
+    bodyStyle:'padding:10px',
+    defaults:{anchor:'100%'},
  
     initComponent: function(){
         // Called during component initialization
+        var config = {
+          items:[
+            {
+              xtype:'combo', 
+              fieldLabel:'External Source',
+              store: new Ext.data.SimpleStore({ fields:['sourceId', 'sourceName'], data:[ [0,'source #1'], [1,'source #2'], [2,'source #3']] }),
+              displayField: 'sourceName',
+              valueField: 'sourceId',
+              mode:'local',
+              listeners:{
+                change: function(){ 
+                  this.fireEvent('sourcechange', this, 1, 2); 
+                }
+              }
+            },
+            {xtype:'combo', fieldLabel:'Filter', disabled:true},
+            {xtype:'textfield', fieldLabel:'Search for', disabled:true},
+            {
+              xtype:'grid', 
+              fieldLabel:'Results',
+              columns: [ {header:'Title'}, {header:'Description'} ],
+              store: new Ext.data.SimpleStore({ fields:['Title', 'Description'], data:[ ['',''], ['',''], ['','']]}),
+              autoExpandColumn:'1',
+              disabled: true
+            }
+          ]          
+        };
  
         // Config object has already been applied to 'this' so properties can 
         // be overriden here or new properties (e.g. items, tools, buttons) 
         // can be added, eg:
-        Ext.apply(this, {
-            propA: 3
-        });
- 
+        Ext.apply(this, config);
+        Ext.apply(this.initialConfig, config); 
+        
         // Before parent code
  
         // Call parent (required)
