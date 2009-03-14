fb0e2785d473b8e22107f36e85815696d1926a35
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-03-14 17:26:42 +0000

    Confirm finding attachment by name

--- test/unit/attachment_test.rb
+++ test/unit/attachment_test.rb
@@ -5,17 +5,26 @@ class AttachmentTest < ActiveSupport::TestCase
   require 'fileutils'
 
   def setup
-    FileUtils.mkdir(Attachment.pwd) if !File.exists(Attachment.pwd)
+    FileUtils.mkdir(Attachment.pwd) if !File.exists?(Attachment.pwd)
   end
 
   def teardown
-    FileUtils.rm_rf(Attachment.pwd) if File.exists(Attachment.pwd)
+    FileUtils.rm_rf(Attachment.pwd) if File.exists?(Attachment.pwd)
   end
 
   # Test if the actual file is created in the expected place
   def test_should_create_new_file
-    @attachment = Attachment.new("#{RAILS_ROOT}/public/images/rails.png", :node_id => '1')
-    @attachment.save
+    attachment = Attachment.new("#{RAILS_ROOT}/public/images/rails.png", :node_id => '1')
+    attachment.save
     assert File.exists?(Attachment.pwd + "/1/1.rails.png")
   end
+
+  # Confirm that an attachment can be found by filename and node_id
+  def test_should_find_file_by_file_name
+    attachment = Attachment.new("#{RAILS_ROOT}/public/images/rails.png", :node_id => '1')
+    attachment.save
+    attachment = Attachment.find("rails.png", :conditions => {:node_id => 1})
+    assert_equal "rails.png", attachment.filename
+  end
+
 end
