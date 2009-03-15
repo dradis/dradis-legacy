b520e581b469f6887a8cf802b8f10ec88b81ee18
Daniel Martin <etd(at)nomejortu.com>
2009-03-15 20:30:07 +0000

    Return a dummy record so we can see how the grid is reloaded

--- vendor/plugins/vulndb_import/lib/vulndb_import.rb
+++ vendor/plugins/vulndb_import/lib/vulndb_import.rb
@@ -7,6 +7,7 @@ module VulndbImport
     module TextSearch
       NAME = 'Search for a specific value in all the fields of the DB'
       def self.run(params={}) 
+        return [{ :title => 'Stub', :description => 'This content was returned by the server.' }]
       end
     end
   end
