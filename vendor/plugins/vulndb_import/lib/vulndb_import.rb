f4af26888ce4ae1d4130f5c776a5af75d0a9b43d
Daniel Martin <etd(at)nomejortu.com>
2009-03-16 14:47:48 +0000

    Fix the typo, we want to access the exception message!

--- vendor/plugins/vulndb_import/lib/vulndb_import.rb
+++ vendor/plugins/vulndb_import/lib/vulndb_import.rb
@@ -30,7 +30,7 @@ module VulndbImport
         rescue Exception => e
           records << { 
                       :title => 'Error fetching records',
-                      :description => message
+                      :description => e.message
                      }
         end
         return records
