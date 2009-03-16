611e66ce8a696cd75e2a3f9c0afc5d47c0965033
Daniel Martin <etd(at)nomejortu.com>
2009-03-16 14:28:38 +0000

    Filter implementation that connects to a remote Vulnerability database to retrieve a list of records matching the user's query (in params[:query]).
    The current implementation asumes that the remote Vulnerability database uses
    the ext_scaffold plugin and submits the query using the facilities provided by
    the plugin.

--- vendor/plugins/vulndb_import/lib/vulndb_import.rb
+++ vendor/plugins/vulndb_import/lib/vulndb_import.rb
@@ -6,8 +6,34 @@ module VulndbImport
   module Filters
     module TextSearch
       NAME = 'Search for a specific value in all the fields of the DB'
+      CONF_FILE = File.join(RAILS_ROOT, 'config', 'vulndb_import.yml')
+      CONF = YAML::load( File.read CONF_FILE )
+
       def self.run(params={}) 
-        return [{ :title => 'Stub', :description => 'This content was returned by the server.' }]
+        records = [] 
+        begin
+          filter_params = {
+            :format => :ext_json,
+            :fields => '["id","vulnerability[title]","vulnerability[body]"]',
+            :query => params[:query]
+          } 
+          Net::HTTP.start(CONF['host'], CONF['port']) do |http|
+            res = http.get("#{CONF['path']}?#{filter_params.to_query}") 
+            records = ActiveSupport::JSON::decode(res.body)['vulnerabilities'].collect do |record|
+              { 
+                :title => record['vulnerability']['title'], 
+                :description => record['vulnerability']['body'] 
+              }
+            end
+          end
+          
+        rescue Exception => e
+          records << { 
+                      :title => 'Error fetching records',
+                      :description => message
+                     }
+        end
+        return records
       end
     end
   end
