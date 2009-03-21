447c5ab7391dc400f005b9b3ece241d090c6dd6e
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-03-21 16:08:59 +0000

    - Handling failure on delete - Enabled multiple selected files delete

--- public/javascripts/attachmentsviewer.js
+++ public/javascripts/attachmentsviewer.js
@@ -14,14 +14,15 @@ dradis.attachments.defaultTemplate= new Ext.XTemplate(
 
 dradis.attachments.deleteAttachment = function () {
     if (attachmentsviewer.fields.dv.getSelectionCount() > 0) {
-        for (x in attachmentsviewer.fields.dv.getSelectedRecords) {
-            var filename = attachmentsviewer.fields.dv.getSelectedRecords()[0].get('filename');
-
+        var selection = attachmentsviewer.fields.dv.getSelectedRecords();
+        for (x in selection) {
+            var filename = selection[x].get('filename');
             Ext.Ajax.request({
                 url: '/nodes/' + attachmentsviewer.currentNode + '/attachments/' + filename,
                 method: 'POST',
                 params: {'_method' : 'delete', 'authenticity_token' : dradis.token},
-                success: function () {attachmentsviewer.fields.dv.store.reload();}
+                success: function () {attachmentsviewer.fields.dv.store.reload();},
+                failure: function () {Ext.Msg.alert('Error', 'The selected file could not be deleted')}
             })
         }
     }
