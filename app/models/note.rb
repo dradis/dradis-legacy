5475bbde6df6d15af0fcc3f9ee92201e5ee5e273
Daniel Martin <etd(at)nomejortu.com>
2009-03-23 20:23:52 +0000

    Standardise the way fields are stored in a note, so the plugins can know in advance. In the future this won't be required as we will have the Fluid Schema Builder, but for the time being, this is the format:
    #[<field 1>]#
    <content for field 1>
    
    #[<field 2>]#
    <content for field 2>
    
    [...]

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,8 @@
 class Note < ActiveRecord::Base
   belongs_to :category
   belongs_to :node
+
+  def fields
+    Hash[ *self.text.scan(/#\[(.+?)\]#\n(.*?)(?=#\[|\z)/m).flatten.collect do |str| str.strip end ]
+  end
 end
