2be4ea94e735c4b1d826f735d085a1d76fd1abdc
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-04-05 21:21:30 +0000

    - The fist passed parameter is the plugin name - we extend it with '_import' if this is not part of the passed string - We need a plugin path (folder name) - we get derive this from the plugin name

--- lib/generators/import_plugin/import_plugin_generator.rb
+++ lib/generators/import_plugin/import_plugin_generator.rb
@@ -1,4 +1,11 @@
 class ImportPluginGenerator < Rails::Generator::NamedBase
+  attr_reader :plugin_path
+
+  def initialize(runtime_args, runtime_options = {})
+    runtime_args[0] = runtime_args[0].tableize + "_export" if !(runtime_args[0].tableize =~ /.*_import/)
+    super
+    @plugin_path = "vendor/plugins/#{file_name}"
+  end
 
   def manifest
     record do |m|
