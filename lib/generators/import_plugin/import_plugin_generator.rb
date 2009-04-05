5527b0643662faba67dcf3f3d008ea39253ca806
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-04-05 21:25:29 +0000

    This will copy all the required files into the created directories

--- lib/generators/import_plugin/import_plugin_generator.rb
+++ lib/generators/import_plugin/import_plugin_generator.rb
@@ -15,6 +15,17 @@ class ImportPluginGenerator < Rails::Generator::NamedBase
       m.directory "#{plugin_path}/tasks"
       m.directory "#{plugin_path}/test"
 
+      m.template 'README',        "#{plugin_path}/README"
+      m.template 'MIT-LICENSE',   "#{plugin_path}/MIT-LICENSE"
+      m.template 'Rakefile',      "#{plugin_path}/Rakefile"
+      m.template 'init.rb',       "#{plugin_path}/init.rb"
+      m.template 'install.rb',    "#{plugin_path}/install.rb"
+      m.template 'uninstall.rb',  "#{plugin_path}/uninstall.rb"
+      m.template 'plugin.rb',     "#{plugin_path}/lib/#{file_name}.rb"
+      m.template 'tasks.rake',    "#{plugin_path}/tasks/#{file_name}_tasks.rake"
+      m.template 'unit_test.rb',  "#{plugin_path}/test/#{file_name}_test.rb"
+      m.template 'meta.rb',       "#{plugin_path}/lib/#{file_name}/meta.rb"
+
     end
   end
 end
