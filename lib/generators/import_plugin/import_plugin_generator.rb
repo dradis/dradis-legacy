542428f02235c1c93b98519935541e9c68fe286f
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-04-05 21:32:39 +0000

    Check to see if the class used by the plugin colides with any other of the Rails classes

--- lib/generators/import_plugin/import_plugin_generator.rb
+++ lib/generators/import_plugin/import_plugin_generator.rb
@@ -2,13 +2,15 @@ class ImportPluginGenerator < Rails::Generator::NamedBase
   attr_reader :plugin_path
 
   def initialize(runtime_args, runtime_options = {})
-    runtime_args[0] = runtime_args[0].tableize + "_export" if !(runtime_args[0].tableize =~ /.*_import/)
+    runtime_args[0] = runtime_args[0].tableize + "_export" if runtime_args[0] && !(runtime_args[0].tableize =~ /.*_import/)
     super
     @plugin_path = "vendor/plugins/#{file_name}"
   end
 
   def manifest
     record do |m|
+      # Check for class naming collisions.
+      m.class_collisions class_path, class_name
 
       m.directory "#{plugin_path}/lib"
       m.directory "#{plugin_path}/lib/#{file_name}"
