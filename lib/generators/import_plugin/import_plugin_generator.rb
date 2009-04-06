27669d719e2ad62f2c518b262d49e7455b7a3538
Daniel Martin <etd(at)nomejortu.com>
2009-04-06 20:00:00 +0000

    Fix a typo, import plugins are for importing stuff and have "import" in their names ;)

--- lib/generators/import_plugin/import_plugin_generator.rb
+++ lib/generators/import_plugin/import_plugin_generator.rb
@@ -2,7 +2,7 @@ class ImportPluginGenerator < Rails::Generator::NamedBase
   attr_reader :plugin_path
 
   def initialize(runtime_args, runtime_options = {})
-    runtime_args[0] = runtime_args[0].underscore + "_export" if runtime_args[0] && !(runtime_args[0].tableize =~ /.*_import/)
+    runtime_args[0] = runtime_args[0].underscore + "_import" if runtime_args[0] && !(runtime_args[0].tableize =~ /.*_import/)
     super
     @plugin_path = "vendor/plugins/#{file_name}"
   end
