ab142a76042bd561302b4575527e1e5745bfa41f
Daniel Martin <etd(at)nomejortu.com>
2009-04-07 10:14:05 +0000

    Add some description and include the "lib" folder in the load path (otherwise internal +requires+ by the modules of the plugin will fail).

--- vendor/plugins/wiki_import/test/wiki_import_test.rb
+++ vendor/plugins/wiki_import/test/wiki_import_test.rb
@@ -1,5 +1,9 @@
 require 'test/unit'
 
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require File.dirname(__FILE__) + '/../init'
+
+# This class contains test cases for the WikiMedia import plugin
 class WikiImportTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def test_this_plugin
