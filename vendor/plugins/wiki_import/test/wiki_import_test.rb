0881f859458c7081d85cfa9644c7558abc821375
Daniel Martin <etd(at)nomejortu.com>
2009-04-07 12:26:36 +0000

    Add a new test that will run the FullTextSearch filter and return a page from the wiki. We still need to use mocha to stub Net::HTTP but the basic structure of the test is already done.

--- vendor/plugins/wiki_import/test/wiki_import_test.rb
+++ vendor/plugins/wiki_import/test/wiki_import_test.rb
@@ -3,6 +3,11 @@ require 'test/unit'
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require File.dirname(__FILE__) + '/../init'
 
+# We need mocha (http://mocha.rubyforge.org/) to stub out Net::HTTP in the test
+# otherwise people testing this library would need to install a MediaWiki on 
+# their own!
+$:.unshift File.dirname(__FILE__) + '/vendor/mocha-0.9.5/lib'
+require 'mocha'
 
 # This class contains test cases for the WikiMedia import plugin
 class WikiImportTest < Test::Unit::TestCase
@@ -53,5 +58,13 @@ EON
   # Note#fields).
   def test_wiki_to_dradis_fields
     assert_equal( DRADIS_NOTE, WikiImport::fields_from_wikitext(WIKI_PAGE) ) 
+  end
+
+  # Connect to a remote wiki, search for a specific string and parse the output
+  # into the format expected by the framework.
+  def test_pull_from_wiki
+    expected = [ {:title => 'Directory Listings', :description => DRADIS_NOTE} ]
+
+    assert_equals( expected, WikiImport::Filters::FullTextSearch.run() )
   end
 end
