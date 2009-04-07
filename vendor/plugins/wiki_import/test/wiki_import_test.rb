c0a4019a1ccb308d5c446edf81558ea3026b2ed3
Daniel Martin <etd(at)nomejortu.com>
2009-04-07 10:19:15 +0000

    Include some reference strings that can be used to ensure that the format conversion was done properly

--- vendor/plugins/wiki_import/test/wiki_import_test.rb
+++ vendor/plugins/wiki_import/test/wiki_import_test.rb
@@ -3,8 +3,50 @@ require 'test/unit'
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require File.dirname(__FILE__) + '/../init'
 
+
 # This class contains test cases for the WikiMedia import plugin
 class WikiImportTest < Test::Unit::TestCase
+  # Reference strings for assertions
+  WIKI_PAGE=<<EOW#nodoc
+=Title=
+Directory Listings
+
+=Impact=
+Low
+
+=Probability=
+Low
+
+=Description=
+Some directories on the server were configured with directory listing enabled. 
+
+This may leak information about the website by revealing files that it uses 
+without requiring the application context in which they are accessed. 
+
+=Recommendation=
+
+Disable directory listings if possible."
+EOW
+  DRADIS_NOTE=<<EON#nodoc
+#[Title]#
+Directory Listings
+
+#[Impact]#
+Low
+
+#[Probability]#
+Low
+
+#[Description]#
+Some directories on the server were configured with directory listing enabled. 
+
+This may leak information about the website by revealing files that it uses 
+without requiring the application context in which they are accessed. 
+
+#[Recommendation]#
+
+Disable directory listings if possible."
+EON
 
   # At some point in the import process we need to translate from 
   # wiki-formatted text into our standard format for notes (see
