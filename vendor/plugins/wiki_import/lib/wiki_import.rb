7cc37c54ef3b860ee2839bdbc4c7cddc14bf8545
Daniel Martin <etd(at)nomejortu.com>
2009-04-07 10:33:27 +0000

    Implement the +fields_from_wikitext+ method to translate from wiki to dradis format

--- vendor/plugins/wiki_import/lib/wiki_import.rb
+++ vendor/plugins/wiki_import/lib/wiki_import.rb
@@ -3,7 +3,20 @@
 require 'wiki_import/filters'
 require 'wiki_import/meta'
 
+# The WikiImport plugin leverages WikiMedia API to extract information from a 
+# wiki (i.e. the 'Reporting Wiki') and import it into a dradis note.
 module WikiImport
+
+  # WikiMedia has its own formatting, and there are some tweaks we need to do 
+  # to addapt it to standard dradis convention.
+  def self.fields_from_wikitext(wikitext)
+    dradis_fields = wikitext
+    fields = ['Title', 'Impact', 'Probability', 'Description', 'Recommendation']
+    fields.each do |f|
+      dradis_fields.sub!( /=#{f}=/, "#[#{f}]#" )
+    end
+    return dradis_fields
+  end
 end
 
 module Plugins
