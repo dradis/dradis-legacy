7efc2747e0446b4d410ca144339c07d217732a08
Daniel Martin <etd(at)nomejortu.com>
2009-04-07 10:16:45 +0000

    Test the conversion from wiki format to dradis note format

--- vendor/plugins/wiki_import/test/wiki_import_test.rb
+++ vendor/plugins/wiki_import/test/wiki_import_test.rb
@@ -5,8 +5,11 @@ require File.dirname(__FILE__) + '/../init'
 
 # This class contains test cases for the WikiMedia import plugin
 class WikiImportTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
+
+  # At some point in the import process we need to translate from 
+  # wiki-formatted text into our standard format for notes (see
+  # Note#fields).
+  def test_wiki_to_dradis_fields
     flunk
   end
 end
