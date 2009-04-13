449a37e1dc113af7443f20521a60274c1429787b
Daniel Martin <etd(at)nomejortu.com>
2009-04-13 22:47:38 +0000

    Patch the wordexport .connected? issue in the release tag

--- vendor/plugins/word_export/init.rb
+++ vendor/plugins/word_export/init.rb
@@ -1,6 +1,6 @@
 # Include hook code here
 require 'word_export'
 
-if (Category.connected? && Category.find_by_name(WordExport::REPORTING_CATEGORY_NAME).nil?)
+if (Category.find_by_name(WordExport::REPORTING_CATEGORY_NAME).nil?)
   Category.new(:name => WordExport::REPORTING_CATEGORY_NAME).save
 end
