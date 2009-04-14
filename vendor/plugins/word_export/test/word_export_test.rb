1a263827afb4ec2c71ef08268bd0889bac6f649b
Daniel Martin <etd(at)nomejortu.com>
2009-04-14 15:37:08 +0000

    First testcase, simple XML paragraph creation, no attributes

--- vendor/plugins/word_export/test/word_export_test.rb
+++ vendor/plugins/word_export/test/word_export_test.rb
@@ -1,8 +1,17 @@
 require 'test/unit'
 
+# require Rails testing framework
+require File.dirname(__FILE__) + '/../../../../test/test_helper'
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require File.dirname(__FILE__) + '/../init'
+
 class WordExportTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+
+  # Simple test to check the paragraph formation facility. A simple text with no
+  # run or paragraph attributes
+  def test_simpletext_noprops
+    expected = '<w:p><w:r><w:t>test text</w:t></w:r></w:p>'
+    assert_equal(expected, WordExport::Processor.word_paragraph_for('test text', {}).to_s)
   end
 end
