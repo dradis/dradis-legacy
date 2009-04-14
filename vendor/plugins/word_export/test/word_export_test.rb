8f51e938f4449caa338512294470a8dd753ad6f2
Daniel Martin <etd(at)nomejortu.com>
2009-04-14 20:44:20 +0000

    Refactor the +test_simpletext_noprops+ to use a constant instead of variable. Introduce a new test for paragraphs that have properties (i.e align right, etc)

--- vendor/plugins/word_export/test/word_export_test.rb
+++ vendor/plugins/word_export/test/word_export_test.rb
@@ -7,11 +7,30 @@ $:.unshift File.dirname(__FILE__) + '/../lib'
 require File.dirname(__FILE__) + '/../init'
 
 class WordExportTest < Test::Unit::TestCase
+  XML_PARA = '<w:p><w:r><w:t>test text</w:t></w:r></w:p>'
+  XML_PARA_PPROPS="<w:p><w:pPr><w:jc w:val='right'/></w:pPr><w:r><w:t>2009-02-10 00:07 GMT</w:t></w:r></w:p>"
+  XML_PARA_PROPS=<<EXP
+  <w:p>
+    <w:pPr>
+      <w:jc w:val="right"/>
+    </w:pPr>
+    <w:r>
+      <w:rPr><w:rFonts w:ascii="Arial" w:h-ansi="Arial" w:cs="Arial"/><wx:font wx:val="Arial"/><w:sz w:val="16"/><w:sz-cs w:val="16"/></w:rPr>
+      <w:t id="vulncreated">2009-02-10 00:07 GMT</w:t>
+    </w:r>
+  </w:p>
+EXP
 
   # Simple test to check the paragraph formation facility. A simple text with no
   # run or paragraph attributes
   def test_simpletext_noprops
-    expected = '<w:p><w:r><w:t>test text</w:t></w:r></w:p>'
-    assert_equal(expected, WordExport::Processor.word_paragraph_for('test text', {}).to_s)
+    assert_equal(XML_PARA, WordExport::Processor.word_paragraph_for('test text', {}).to_s)
+  end
+
+  def test_pprops
+    pprops = [
+      { :root => 'w:jc', :attributes => { 'w:val' => 'right' } }
+    ]
+    assert_equal(XML_PARA_PPROPS, WordExport::Processor.word_paragraph_for('2009-02-10 00:07 GMT', :pprops => pprops).to_s)
   end
 end
