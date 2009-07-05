d23640770f99bd167da082e4de621cbd4528b0c9
Daniel Martin <etd(at)nomejortu.com>
2009-07-05 17:36:38 +0000

    Port the client side extension to a server side plugin, it only does XML imports for the time being buit is stil impressive ;)

--- vendor/plugins/nmap_upload/lib/nmap_upload/filters.rb
+++ vendor/plugins/nmap_upload/lib/nmap_upload/filters.rb
@@ -1,27 +1,81 @@
 module NmapUpload  
-  
-  # complete this with the different filters that your import plugin defines
-  module Filters
 
-    # Dummy filter that can be deleted
-    module SearchInArray
-      NAME = "Dummy filter: Search for a word in a static array (['red', 'blue', 'green']"
-      CONF = {
-        :list => ['red', 'blue', 'green']
-      }
-      def self.run(params={})
-        records = []
-        if (CONF[:list].include?(params[:query]))
-          records << { :title => 'Search found!', :description => 'The search term was found in the list' }
-        else
-          records << { :title => 'Not found', :description => 'The search term was not in the list' }
-        end
-        records << {:title => 'Filter information', :description => "This filter is implemented in #{__FILE__}"}
-        return records
+  private
+  def self.parse_xml_output(content)
+    # initiate some variables to be used
+    hosts = {}
+    xml = REXML::Document.new(content.to_s)
+      
+    # loop through all hosts
+    REXML::XPath.match(xml[6],"//host").each do |host|
+      hostnodename = host.elements['address'].attributes['addr']
+      if (!host.elements['hostnames'].elements.empty?)
+       hostnodename << '('
+       host.elements['hostnames'].elements.to_a.collect do |hostname|
+         hostname.attributes['name']
+       end.join(",")
+       hostnodename << ')'
+      end
+
+      hosts[hostnodename] = {:notes => host.to_s}
+      hosts[hostnodename][:ports] = {}
+
+      REXML::XPath.match(host.elements['ports'], "port").each do |port|
+        hosts[hostnodename][:ports][port.attributes['portid']] = {
+          :protocol => port.attributes['protocol'],
+          :state => port.attributes['state'],
+          :service => port.elements['service'].attributes['name']
+        }
       end
     end
- 
-    # Your filters go here
+    
+    return hosts
+  end
 
+  public
+  
+  # The framework will call this function if the user selects this plugin from
+  # the dropdown list and uploads a file.
+  # @returns true if the operation was successful, false otherwise
+  def self.import(params={})
+    file_content = File.read( params[:file].fullpath )
+    hosts = parse_xml_output(file_content)
+
+    # get the "nmap output" category instance or create it if it does not exist
+    category = Category.find_by_name('Nmap output') 
+    if (category.nil?)
+      category = Category.new( :name => 'Nmap output')
+      category.save
+    end
+
+    port_notes_to_add = {}
+    hosts.each do |host, detail|
+      host_node = Node.new( :label => host)
+      host_node.save
+
+      # add the nmap output for the host as notes to the node
+      Note.new(
+        :node_id => host_node.id,
+        :author => 'Nmap',
+        :category_id => category.id,
+        :text => detail[:notes]
+      ).save
+
+      detail[:ports].each do |port, detail|
+        # Add a node for the port
+        port_node = Node.new( :parent_id => host_node.id, :label => "#{port}/#{detail[:protocol]}" )
+        port_node.save
+
+        # add a note with the port information
+        Note.new(
+          :node_id => port_node.id,
+          :author => 'Nmap',
+          :category_id => category.id,
+          :text => "State: #{detail[:state]}, Service: #{detail[:service]}"
+        ).save
+      end
+    end
+
+    return true
   end
 end
