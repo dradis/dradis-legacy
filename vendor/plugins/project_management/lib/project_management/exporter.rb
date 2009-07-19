2870c6f593f0a9408a621abc811b71e1d356dcc5
Daniel Martin <etd(at)nomejortu.com>
2009-07-19 13:05:27 +0000

    Add a new Processor class that will implement the operations. Splitting the implementation from the Actions is a good idea so we can have Rake tasks or other plugins accessing these methods

---
+++ vendor/plugins/project_management/lib/project_management/exporter.rb
@@ -0,0 +1,10 @@
+module ProjectExport
+  # The Processor class does the heavy-lifting of the export functionalities
+  # provided by the ProjectManagement plugin.
+  # The different Actions defined in this module will in turn call methods in
+  # the Processor class to implement the requested operations
+  class Processor
+    private
+    public
+  end
+end
