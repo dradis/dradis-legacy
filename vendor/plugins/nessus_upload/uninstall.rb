e3a0078fff84a2aa34c569219a59c78145385d1b
Daniel Martin <etd(at)nomejortu.com>
2009-07-22 11:06:11 +0000

    Create uninstall hooks that would remove any configuration file created by the plugin

--- vendor/plugins/nessus_upload/uninstall.rb
+++ vendor/plugins/nessus_upload/uninstall.rb
@@ -1,1 +1,15 @@
 # Uninstall hook code here
+
+for path in [ ['config', 'nessus_upload.yml'] ]
+  full_path = File.join(RAILS_ROOT,*path)
+  print "  #{path.join('/')} "
+
+  if File.exists?(fullpath)
+    print 'exists, it was created by the plugin and is no longer needed, remove [yN]? '
+    if gets("\n").chomp.downcase.first == 'y'
+      File.delete( full_path )
+    else
+      puts "    ...skipped";
+    end
+  end
+end
