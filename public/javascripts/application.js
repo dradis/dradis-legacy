f6e82250d770c47445caf7b91afa8a9af7e3b3a9
Daniel Martin <etd(at)nomejortu.com>
2009-07-25 15:14:51 +0000

    Include de expand/collapse functions required by the project manager. The code is the same used in the meta-server package.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,42 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+/* 
+ * from the meta-server project browser: 
+ * https://dradis.svn.sourceforge.net/svnroot/dradis/meta-server/trunk/public/javascripts/application.js
+ */
+function collapseProject(id) {
+  var els = document.getElementsByClassName(id, 'browser');
+  for (var i = 0; i < els.length; i++) {
+    Element.hide(els[i]);
+  }
+  $(id).removeClassName('open');
+}
+
+function expandProject(id) {
+  var els = document.getElementsByClassName(id, 'browser');
+  for (var i = 0; i < els.length; i++) {
+    Element.show(els[i]);
+  }
+  $(id).addClassName('open');
+}
+
+function projectClick(id) {
+    el = $(id);
+    if (el.hasClassName('open')) {
+        collapseProject(id);
+        el.addClassName('collapsed');
+        return false;
+    //} else if (el.hasClassName('loaded')) {
+    } else if (el.hasClassName('collapsed')) {
+        expandProject(id);
+        el.removeClassName('collapsed');
+        return false;
+    }
+    /* no Ajax yet
+    if (el.hasClassName('loading')) {
+        return false;
+    }
+    el.addClassName('loading');
+    return true;
+    */
+}
+
+
