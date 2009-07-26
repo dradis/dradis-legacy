9c8b341fc967de00b388d2e797a5b1eeee0f5aff
Daniel Martin <etd(at)nomejortu.com>
2009-07-26 19:00:59 +0000

    Move all the validation code for the setup() action to helper filters so we can focus on properly seting up the project once we hit the action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   layout 'banner'
   
   before_filter :check_test_password, :only => :new
+
+  # Validate user settings before seting up the new project
+  before_filter :update_user_selection, :only => :setup
+  before_filter :ensure_valid_password, :only => :setup
+  before_filter :ensure_valid_metaserver_settings, :only => :setup
   
   # ------------------------------------------------------------- test password
   # render init.rhtml
@@ -41,98 +46,22 @@ class SessionsController < ApplicationController
     end
   end
   
+  # Once the user submits the settings form we initialise the database, note 
+  # that the ensure_valid_password and ensure_valid_metaserver_settings filters
+  # have performed the necessary validation of the supplied input
   def setup
     unless Configuration.password.match('dradis')
       redirect_to :action => :new
       return
     end
 
-    mode = params.fetch(:mode, nil)
-    # Just in case validation fails, ensure that the checkboxes have the right
-    # selection
-    session[:meta_server] = nil if (mode == 'new')
-    @new_project = session[:meta_server] ? false : true
-
-    # Step 1:  Password and Password confirmation match
-    pwd = params.fetch( :password, nil )
-    if (pwd.nil?)
-      flash[:error] = 'You need to provide new password information.'
-      redirect_to :action => :init
-      return
-    end
-    
-    pwd1 = pwd.fetch( :value, nil )
-    pwd2 = pwd.fetch( :confirm_value, nil )
-    
-    if (pwd1.nil? || pwd2.nil? || pwd1.blank?)
-      flash[:error] = 'You need to provide both a password and a confirmation.'
-      redirect_to :action => :init
-      return
-    end
-    
-    if not pwd1 == pwd2
-      flash[:error] = 'The password did not match the confirmation.'
-      redirect_to :action => :init
-      return      
-    end
-    
-    # Step 2: Ensure that we have a Revision, if the user has chosen meta-server mode
-    revision = params.fetch(:revision, nil)
-
-    if (mode.nil? || ((mode!='meta-server') & (mode!='new')))
-      flash[:error] = 'You have to choose a valid mode'
-      redirect_to :action => :init
-      return      
-    end
-
-    if (mode == 'meta-server') && revision.nil? 
-      flash[:error] = 'You have to choose a revision to checkout'
-      redirect_to :action => :init
-      return      
-    end
-
-    if (mode == 'meta-server') && session[:meta_server].nil?
-      # TODO: this should never happen!!
-      #flash[:error] = 'You have to choose a revision to checkout'
-      redirect_to :action => :init
-      return      
-    end
-    
-    
-    project_revision = nil
-    if ( !@new_project )
-      project, revision = revision.split('_')
-      p_id = project.to_i
-      r_id = revision.to_i
-      begin
-        Project.find_from_metaserver( session[:meta_server] )
-        project = Project.find(p_id)
-        revision_found = false
-        project.attributes['revisions'].each do |rev|
-          next if (rev.id != r_id)
-          revision_found = true
-          project_revision = rev
-        end
-
-        if !revision_found
-          flash[:error] = 'Invalid revision'
-          redirect_to :action => :init
-          return      
-        end
-      rescue
-          flash[:error] = 'Invalid revision'
-          redirect_to :action => :init
-          return      
-      end
-    end
-
+   
     # Step 3: Initialise the project
+    # @password was set by the ensure_valid_password filter
     c = Configuration.find_by_name('password')
-    c.value = pwd1
+    c.value = @password 
     c.save
 
-    # Download project revision
-    
     flash[:notice] =  'Password set. Please log in.<br/> Remember to adjust the client configuration file (client/conf/dradis.xml).'
     redirect_to :action => :new
   end
@@ -166,7 +95,7 @@ class SessionsController < ApplicationController
 
   protected
   # before filter, if the database doesn't contain a valid password, a new
-  # one is created.  
+  # one is created.  
   def check_test_password
     if Configuration.password.match('dradis')
       redirect_to :action => :init
@@ -186,5 +115,104 @@ class SessionsController < ApplicationController
       first_login = true
     end
     return first_login
+  end
+
+  # Ensure that we keep the choosen preferences by the user, just in case a 
+  # validation does fail, the user should still be presented with the right
+  # interface
+  def update_user_selection
+    @mode = params.fetch(:mode, nil)
+    # Just in case validation fails, ensure that the checkboxes have the right
+    # selection
+    session[:meta_server] = nil if (@mode == 'new')
+    @new_project = session[:meta_server] ? false : true
+  end
+
+  # Ensure that the user has provided a valid password, that the password 
+  # matches the confirmation and that they are not empty.
+  def ensure_valid_password
+    # Step 1:  Password and Password confirmation match
+    pwd = params.fetch( :password, nil )
+    if (pwd.nil?)
+      flash[:error] = 'You need to provide new password information.'
+      redirect_to :action => :init
+      return false
+    end
+    
+    pwd1 = pwd.fetch( :value, nil )
+    pwd2 = pwd.fetch( :confirm_value, nil )
+    
+    if (pwd1.nil? || pwd2.nil? || pwd1.blank?)
+      flash[:error] = 'You need to provide both a password and a confirmation.'
+      redirect_to :action => :init
+      return false
+    end
+    
+    if not pwd1 == pwd2
+      flash[:error] = 'The password did not match the confirmation.'
+      redirect_to :action => :init
+      return false
+    end
+ 
+    @password = pwd1
+    return true
+  end
+
+  # Check that a user has choosen a revision if running in meta-server @mode
+  def ensure_valid_metaserver_settings
+    # Step 2: Ensure that we have a Revision, if the user has chosen meta-server mode
+    revision = params.fetch(:revision, nil)
+
+    if (@mode.nil? || ((@mode!='meta-server') & (@mode!='new')))
+      flash[:error] = 'You have to choose a valid mode'
+      redirect_to :action => :init
+      return false
+    end
+
+    if (@mode == 'meta-server') && revision.nil? 
+      flash[:error] = 'You have to choose a revision to checkout'
+      redirect_to :action => :init
+      return false
+    end
+
+    if (@mode == 'meta-server') && session[:meta_server].nil?
+      # TODO: this should never happen!!
+      #flash[:error] = 'You have to choose a revision to checkout'
+      redirect_to :action => :init
+      return      
+    end
+    
+    
+    @project_revision = nil
+    if ( !@new_project )
+      project, revision = revision.split('_')
+      p_id = project.to_i
+      r_id = revision.to_i
+      begin
+        # FIXME: Split find_from_metaserver into 'setup' and 'find' steps so we
+        #   can use the 'setup' here
+        Project.find_from_metaserver( session[:meta_server] )
+        project = Project.find(p_id)
+        revision_found = false
+        project.attributes['revisions'].each do |rev|
+          next if (rev.id != r_id)
+          revision_found = true
+          @project_revision = rev
+        end
+
+        if !revision_found
+          flash[:error] = 'Invalid revision'
+          redirect_to :action => :init
+          return      
+        end
+      rescue
+          flash[:error] = 'Invalid revision'
+          redirect_to :action => :init
+          return      
+      end
+    end
+   
+    p @project_revision
+    return true
   end
 end
