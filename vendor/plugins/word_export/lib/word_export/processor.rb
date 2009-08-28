aa53012dfdd0049da29c8c877bb9112a05936fc4
Daniel Martin <etd(at)nomejortu.com>
2009-08-28 08:26:11 +0000

    Now you can use the logger module-wide, it may be worth thinking about providing a Plugins::logger facility...

--- vendor/plugins/word_export/lib/word_export/processor.rb
+++ vendor/plugins/word_export/lib/word_export/processor.rb
@@ -11,6 +11,7 @@ module WordExport
       :logger_name => 'WordExport',
       # TODO: can we do better than this? i.e. __FILE__ ?
       :template => './vendor/plugins/word_export/template.xml',
+      :template => '/tmp/final-template.xml',
       #FIXME:category_name => REPORTING_CATEGORY_NAME,
       :required_fields => ['Title', 'Description', 'Recommendation'],
     }
