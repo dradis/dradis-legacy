88db75289496266d2592761a6f7ecf4eb83e0e32
Daniel Martin <etd(at)nomejortu.com>
2009-09-04 22:37:39 +0000

    Provide install/uninstall hooks that will copy the config file from the plugin directory into the standard RAILS_ROOT/config/ folder.
    On uninstallation, if the config file has not been modified it will be removed,
    if it has, then the user will be prompted and will have to decide what to do.

--- lib/generators/import_plugin/templates/uninstall.rb
+++ lib/generators/import_plugin/templates/uninstall.rb
@@ -1,1 +1,24 @@
 # Uninstall hook code here
+
+for path in [ ['config', '<%= file_name %>.yml'] ]
+  original = File.join(File.dirname(__FILE__),*path)
+  used = File.join(RAILS_ROOT,*path)
+
+  print "  #{path.join('/')} "
+  if File.exists?(used)
+    if FileUtils.cmp(original, used)
+        File.delete( used )
+        puts "done"
+    else
+      print "has been modified but is no longer needed, delete [yN]?"
+      if STDIN.gets("\n").chomp.downcase[0] == ?y
+        File.delete( used )
+        puts "done"
+      else
+        puts "    ...skipped"; next
+      end
+    end
+  else
+    puts "was not found."
+  end
+end
