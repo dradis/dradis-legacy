00005d89404413a487f7a853504ab03da56364bd
Daniel Martin <etd(at)nomejortu.com>
2009-09-04 22:46:20 +0000

    Provide install/uninstall hooks that would copy the template config file into RAILS_ROOT/config/ and remove it once the plugin is uninstalled

--- vendor/plugins/osvdb_import/uninstall.rb
+++ vendor/plugins/osvdb_import/uninstall.rb
@@ -1,1 +1,24 @@
 # Uninstall hook code here
+
+for path in [ ['config', 'osvdb_import.yml'] ]
+  original = File.join(File.dirname(__FILE__),*path)
+  used = File.join(RAILS_ROOT,*path)
+
+  print "  #{path.join('/')} "
+  if File.exists?(used)
+    if FileUtils.cmp(original, used)
+        File.delete( used )
+        puts "done"
+    else
+      print "has been modified but is no longer needed, delete [yN]?"
+      if STDIN.gets("\n").chomp.downcase[0] == ?y
+        File.delete( used )
+        puts "done"
+      else
+        puts "    ...skipped"; next
+      end
+    end
+  else
+    puts "was not found."
+  end
+end
