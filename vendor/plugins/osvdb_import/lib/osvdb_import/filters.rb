c9dc936a74ccf9098b1f0a8efa0714f7c2493096
Daniel Martin <etd(at)nomejortu.com>
2009-09-07 17:18:58 +0000

    - Fix a typo in the filter name, it should have been "OSVDB"
    - The config file is common across filters so move it to the Filters module

--- vendor/plugins/osvdb_import/lib/osvdb_import/filters.rb
+++ vendor/plugins/osvdb_import/lib/osvdb_import/filters.rb
@@ -14,6 +14,9 @@ module OSVDBImport
   
   # complete this with the different filters that your import plugin defines
   module Filters
+    BAD_API_KEY = '<your_API_key>'
+    CONF_FILE = File.join(RAILS_ROOT, 'config', 'osvdb_import.yml')
+    CONF = YAML::load( File.read CONF_FILE )
 
     # GeneralSearch Filter: Internally uses the "Find Vulns by custom query" 
     # API to get vulnerabilities by a custom query. Returns a maximum of 30 
@@ -21,31 +24,29 @@ module OSVDBImport
     # Sample Query for "XSS"  http://osvdb.org/api/vulns_by_custom_search/<your_API_key>/?request=XSS&order=osvdb_id
     module GeneralSearch
       NAME = "General Search"
-      CONF_FILE = File.join(RAILS_ROOT, 'config', 'vulndb_import.yml')
-      CONF = YAML::load( File.read CONF_FILE )
-      CONF = {
-        :list => ['red', 'blue', 'green']
-      }
+
       
       def self.run(params={})
+        p CONF
+
+        logger = params.fetch( :logger, RAILS_DEFAULT_LOGGER )        
+        query = params.fetch( :query, '')
+        url = "http://osvdb.org/api/vulns_by_custom_search/<your_API_key>/?request=#{CGI::escape(query)}&order=osvdb_id"
+
+        logger.info{ "Running a general search in the OSVDB with the query: #{query}" }
+        logger.debug{ "URL is: #{url}" }
         
-        records = []
-        if (CONF[:list].include?(params[:query]))
-          records << { :title => 'Search found!', :description => 'The search term was found in the list' }
-        else
-          records << { :title => 'Not found', :description => 'The search term was not in the list' }
-        end
-        records << {:title => 'Filter information', :description => "This filter is implemented in #{__FILE__}"}
-        return records
       end
     end
  
     # OSVDIDLookup: run a search looking for a specific OSVDID
     #
     # Sample Query for osvdb_id 1234:  http://osvdb.org/api/find_by_osvdb/<your_API_key>/1234
-    module OSVDIDLookup
+    module OSVDBIDLookup
       NAME = 'OSVDB ID Lookup' 
+
       def self.run(params={})
+
       end
     end
     
