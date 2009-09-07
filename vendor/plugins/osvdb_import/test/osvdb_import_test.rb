1905c634f3f5885d53141a34479ea4a1c162ff25
Daniel Martin <etd(at)nomejortu.com>
2009-09-07 19:06:30 +0000

    First unit test, ensure that a valid API key has been configured in the config file.
    And verify that ALL filters detect when this is not the case.

--- vendor/plugins/osvdb_import/test/osvdb_import_test.rb
+++ vendor/plugins/osvdb_import/test/osvdb_import_test.rb
@@ -1,8 +1,30 @@
 require 'test/unit'
 
+# require Rails testing framework
+require File.dirname(__FILE__) + '/../../../../test/test_helper'
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require File.dirname(__FILE__) + '/../init'
+
+
 class OSVDBImportTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  # Check that the user has configured an API key
+  def test_no_API_key
+    OSVDBImport::Filters::CONF['API_key'] = OSVDBImport::Filters::BAD_API_KEY
+
+    assert_raise RuntimeError do
+      OSVDBImport::Filters::validate_API_key()
+    end
+
+    [ 
+      OSVDBImport::Filters::OSVDBIDLookup, 
+      OSVDBImport::Filters::GeneralSearch 
+    ].each do |filter|
+      results = filter::run(:query => '1234')
+      assert_equal( 1, results.size, 'Only one result was expected (the error message)')
+
+      assert_equal( 'Error in OSVDB API key', results[0][:title], 'Unknown error when expecting an API key error' )
+    end
   end
+
 end
