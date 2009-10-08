d87f60a19bc04913578465ea64e12578e589f186
Daniel Martin <etd(at)nomejortu.com>
2009-10-08 14:00:57 +0000

    Simplify!

--- vendor/plugins/word_export/init.rb
+++ vendor/plugins/word_export/init.rb
@@ -1,6 +1,4 @@
 # Include hook code here
 require 'word_export'
 
-if (Category.find_by_name(WordExport::REPORTING_CATEGORY_NAME).nil?)
-  Category.new(:name => WordExport::REPORTING_CATEGORY_NAME).save
-end
+Category.find_or_create_by_name( WordExport::REPORTING_CATEGORY_NAME )
