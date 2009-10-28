067d0aef9a2533c5cb1fa533922fe8d78ea11986
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-10-28 22:21:45 +0000

    Very basic method with the minimal data to serve the rss feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,12 @@ class HomeController < ApplicationController
   
   def index
   end
+
+  def rss
+    # this contains the list of items to be displayed in rss
+    # TODO: where do we get this from, yes the revision 
+    @items = []
+    response.headers['Content-Type'] = 'application/rss+xml'
+    render :action => 'rss', :layout => false
+  end
 end
