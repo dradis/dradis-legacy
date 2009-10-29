b8efd5d5682c170478cf61e2bc617b4662fb9ae3
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-10-29 21:34:59 +0000

    Extracting a value from the record to be used in the rss

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -1,7 +1,21 @@
 class RssFeed < ActiveRecord::Base
 
+  RssValueAccessors = [:name, :title, :label, :text]
+
   def title
     "#{self.action.humanize} a #{self.resource} on #{self.actioned_at.strftime("%Y-%m-%d %H:%M:%S")}"
+  end
+
+  def description
+    "#{self.value}"
+  end
+
+  def self.extract_rss_value(record)
+    RssValueAccessors.each do |accessor|
+      next unless record.respond_to? accessor
+      return record.send accessor
+    end
+    return nil
   end
 
 end
