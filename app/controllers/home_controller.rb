fe2a230e8d3b6e45a59cee6aba36fa07afee1fa6
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-10-29 20:12:15 +0000

    Assembing rss items from db content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,22 @@ class HomeController < ApplicationController
   end
 
   def rss
-    # this contains the list of items to be displayed in rss
-    # TODO: where do we get this from, yes the revision 
+    observed_models = [:node, :note, :category]
+    # this contains the list of items to be displayed in rss 
     @items = []
+    observed_models.each do |model|
+      elements = model.to_s.classify.constantize.find(:all, :order => "updated_at DESC", :limit => 10)
+      elements.each do |element|
+        title = element.updated_at == element.created_at ? "Created #{model.to_s}" : "Update #{model.to_s}"
+        title << " on #{element.updated_at}"
+        @items << {:date => element.updated_at,
+          :title => title
+        }
+      end
+    end
+    @items.sort {|a,b| a[:date] <=> b[:date]}
+    @items = @items[0..9]
+
     response.headers['Content-Type'] = 'application/rss+xml'
     render :action => 'rss', :layout => false
   end
