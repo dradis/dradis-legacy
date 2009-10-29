69c092c902d5e8d3d105f17a3129eae820191b0c
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-10-29 21:56:05 +0000

    Changing to feeds instead of rss_feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,7 @@
 class FeedsController < ApplicationController
 
   def index
-    @rss_feeds = Feed.find(:all, :limit => 20)
+    @feeds = Feed.find(:all, :limit => 20)
 
     response.headers['Content-Type'] = 'application/rss+xml'
     render :action => 'index', :layout => false
