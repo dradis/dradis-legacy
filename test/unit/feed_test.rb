80ca953c3e51ca1eb02424a81fe23afd46973d43
Siebert Lubbe <siebertlubbe(at)googlemail.com>
2009-10-29 21:45:11 +0000

    Changing to feeds instead of rss_feeds

--- test/unit/feed_test.rb
+++ test/unit/feed_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class RssFeedsTest < ActiveSupport::TestCase
+class FeedsTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
