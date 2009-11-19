66c1dd27e296fd455fc7adaa7dc132c20b993fe1
Daniel Martin <etd(at)nomejortu.com>
2009-11-19 16:13:15 +0000

    Update the schema file with the table and columns for the RSS feed table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(:version => 5) do
 
   create_table "configurations", :force => true do |t|
     t.string   "name"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "feeds", :force => true do |t|
+    t.string   "action"
+    t.string   "user"
+    t.datetime "actioned_at"
+    t.string   "resource"
     t.string   "value"
     t.datetime "created_at"
     t.datetime "updated_at"
