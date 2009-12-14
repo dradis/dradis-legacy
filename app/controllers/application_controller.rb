c437f1f74e83758fa1dd04c57f158fb70c104d11
Daniel Martin <etd(at)nomejortu.com>
2009-12-14 20:11:56 +0000

    If running for the first time, show the wizard. Some thought needs to go to what to do with the current code that presents the 'extra tab' on first login and every time the user clicks on the *what's new in this version?* button.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,24 @@ class ApplicationController < ActionController::Base
   
   filter_parameter_logging :password, :password_confirmation
 
+  before_filter :show_first_time_wizard
+
+  # This filter would display the 'First Time' wizard the first time dradis is
+  # run in this box. 
+  # In order to do this it checks for the presence of the 
+  # config/first_login.txt file and if not found, it creates it and presents
+  # the user with the wizard view.
+  def show_first_time_wizard
+    magic_file = File.join( RAILS_ROOT, 'config', 'first_login.txt' )
+    if (File.exists?(magic_file) )
+      return true
+    else
+      File.open(File.join(RAILS_ROOT, "config/first_login.txt"), "w") do |f|
+        f << "This file indicates that a succesful login event has occurred on this dradis instance"
+      end
+
+      redirect_to :controller => :wizard
+    end
+  end
+
 end
