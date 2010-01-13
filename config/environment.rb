77717f9eb67945fb108afa33a4ce012577555b79
Daniel Martin <etd(at)nomejortu.com>
2010-01-13 12:49:22 +0000

    Add the rack-1.0.1 gem to the vendor/ directory. Otherwise people downloading the app from the repository will have trouble running it as Rails depends on Rack but the gem in not installed as a dependency or frozen with the other dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'rack', :version => '~>1.0.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
