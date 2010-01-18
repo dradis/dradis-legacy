f35d96094eb4cca824c0d5b1842088dba6a8b53e
Daniel Martin <etd(at)nomejortu.com>
2010-01-18 20:50:20 +0000

    Only fetch the Notes associated with our reporting category

--- vendor/plugins/html_export/lib/html_export/actions.rb
+++ vendor/plugins/html_export/lib/html_export/actions.rb
@@ -6,8 +6,12 @@ module HTMLExport
     #
     # It uses the template at: ./vendor/plugins/html_export/template.html.erb
     def to_html(params={})
+      category_name = params.fetch(:category_name, REPORTING_CATEGORY_NAME)
+      reporting_cat = Category.find_by_name(category_name)
+      reporting_notes_num = Note.count(:all, :conditions => {:category_id => reporting_cat})
+
       title = "Dradis Framework - v#{Core::VERSION::STRING}"
-      notes = Note.all 
+      notes = Note.find( :all, :conditions => {:category_id => reporting_cat} )
       erb = ERB.new( File.read(HTMLExport::CONF[:template]) )
       #send( erb.result(),  :filename => 'report.html', :type => 'text/html') 
       render :type => 'text/html', 
