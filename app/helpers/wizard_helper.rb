711b5efb8c3fee38024c4e6b9244661ea400e462
Daniel Martin <etd(at)nomejortu.com>
2010-01-19 19:11:51 +0000

    Although this is not the cleanest solution, replace the static tabs menu with one generated with a helper that detects the current tab and assigns the *active* CSS class to the corresponding list element.

--- app/helpers/wizard_helper.rb
+++ app/helpers/wizard_helper.rb
@@ -1,2 +1,18 @@
 module WizardHelper
+  def menu_tabs
+    [ 
+      ['Welcome', 'index'], 
+      ['Users and Passwords', 'users' ],
+      ['Interface', 'interface' ],
+      [ 'Plugins', 'plugins' ],
+      [ 'Community / Help', 'community' ]
+    ].collect do |title, link|
+      action = request.parameters.fetch('action', '')
+      if (action == link)
+        "<li class=\"active\"><a href=\"#\"><span>#{title}</span></a></li>"  
+      else
+        "<li><a href=\"/wizard/#{link}\"><span>#{title}</span></a></li>"  
+      end
+    end.join
+  end
 end
