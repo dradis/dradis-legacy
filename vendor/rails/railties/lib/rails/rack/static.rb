8842df525d415b9cafc2460b9974ddba780d540e
Daniel Martin <etd(at)nomejortu.com>
2010-07-15 21:49:16 +0000

    Improve the performance of the app by adding 'Cache-Control' and 'Expires' tags to the different assets (images, javascripts and CSSs).
    Since rails is using Rails::Rack::Static to serve assests we cannot configure a
    standard Rack middleware (we would do it in config/environment.rb with a line
    like config.middleware.use). Instead we have to modify Rails::Rack::Static to
    include the cache-control headers. Inspiration was drawn from Racl::StaticCache
    of the rack-contrib package (see [i]).
    
    [i]
    http://github.com/rack/rack-contrib/raw/master/lib/rack/contrib/static_cache.rb

--- vendor/rails/railties/lib/rails/rack/static.rb
+++ vendor/rails/railties/lib/rails/rack/static.rb
@@ -8,6 +8,9 @@ module Rails
       def initialize(app)
         @app = app
         @file_server = ::Rack::File.new(File.join(RAILS_ROOT, "public"))
+        @cache_duration = 1
+        @duration_in_seconds = self.duration_in_seconds
+        @duration_in_words    = self.duration_in_words
       end
 
       def call(env)
@@ -16,7 +19,14 @@ module Rails
 
         if FILE_METHODS.include?(method)
           if file_exist?(path)
-            return @file_server.call(env)
+            status, headers, body = @file_server.call(env)
+            headers['Cache-Control'] ="max-age=#{@duration_in_seconds}, public"
+            headers['Expires'] = @duration_in_words
+            headers.delete 'Etag'
+            headers.delete 'Pragma'
+            headers.delete 'Last-Modified'
+p headers
+            return [status, headers, body]
           else
             cached_path = directory_exist?(path) ? "#{path}/index" : path
             cached_path += ::ActionController::Base.page_cache_extension
@@ -31,6 +41,14 @@ module Rails
         @app.call(env)
       end
 
+      def duration_in_words
+        (Time.now + self.duration_in_seconds).strftime '%a, %d %b %Y %H:%M:%S GMT'
+      end
+
+      def duration_in_seconds
+        60 * 60 * 24 * 365 * @cache_duration
+      end
+
       private
         def file_exist?(path)
           full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
@@ -41,6 +59,7 @@ module Rails
           full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
           File.directory?(full_path) && File.readable?(full_path)
         end
+
     end
   end
 end
