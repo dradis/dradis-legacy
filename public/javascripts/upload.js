4541c1f428c5416e99857c33973270895cbcbacf
Daniel Martin <etd(at)nomejortu.com>
2010-09-06 18:42:58 +0000

    * Convert the main form into an "ajax" form, although this is doing a the iframe trick in the background * When the file input changes, create some new elements in the DOM and submit the form.
    The new elements will be used as skeleton for the information of the newly 
    uploaded file. When the upload completes, we will fill in this skeleton and then
    we will use Ajax to update the page with the latest information passed by the 
    Upload plugin processor.

---
+++ public/javascripts/upload.js
@@ -0,0 +1,15 @@
+jQuery.ajaxSetup({
+  'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+});
+
+$(function(){
+  $('form').ajaxForm({dataType: 'script'});
+
+  $(':file').change(function(){
+    $('#files').empty();
+    var itemId = 'file_' + new Date().getTime();
+    $('#files').prepend('<div id="' + itemId + '" class="file">\n<div><strong>Filename</strong>: ' + this.value + ' <span><img style="margin:0;" src="/images/loading.gif" /></span></div>\n<div><strong>Size</strong>: </div>\n<blockquote></blockquote></div>');
+    $('#item_id').val(itemId);
+    $(this).closest('form').submit();
+  });
+});
