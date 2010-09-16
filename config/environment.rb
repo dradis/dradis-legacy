98257daa80b28c6e6b270ec40d49ef37641dc926
Daniel Martin <etd(at)nomejortu.com>
2010-09-16 12:09:16 +0000

    Add the delayed_jobs gem for background processing of the file uploads

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'rack', :version => '~>1.0.1'
+  config.gem 'delayed_job'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
---
+++ vendor/gems/delayed_job-2.0.3/spec/spec_helper.rb
@@ -0,0 +1,26 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+
+require 'rubygems'
+require 'spec'
+require 'logger'
+
+require 'delayed_job'
+require 'sample_jobs'
+
+Delayed::Worker.logger = Logger.new('/tmp/dj.log')
+RAILS_ENV = 'test'
+
+# determine the available backends
+BACKENDS = []
+Dir.glob("#{File.dirname(__FILE__)}/setup/*.rb") do |backend|
+  begin
+    backend = File.basename(backend, '.rb')
+    require "setup/#{backend}"
+    require "backend/#{backend}_job_spec"
+    BACKENDS << backend.to_sym
+  rescue LoadError
+    puts "Unable to load #{backend} backend! #{$!}"
+  end
+end
+
+Delayed::Worker.backend = BACKENDS.first
