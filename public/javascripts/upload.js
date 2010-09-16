70bd4e578b47b2246d07e30927f48b09d9446176
Daniel Martin <etd(at)nomejortu.com>
2010-09-16 12:57:35 +0000

    The @item_id@ is now a number instead of "file_<number>" this is so we can use it both here and as the @uid@ value in the database when generating the Log entries for this upload.
    And instead of passing it as the div's id attribute we use HTML5 data-id

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -7,9 +7,10 @@ $(function(){
 
   $(':file').change(function(){
     $('#files').empty();
-    var itemId = 'file_' + new Date().getTime();
-    $('#files').prepend('<div id="' + itemId + '" class="file">\n<div><strong>Filename</strong>: ' + this.value + ' <span><img style="margin:0;" src="/images/loading.gif" /></span></div>\n<div><strong>Size</strong>: </div>\n<blockquote></blockquote></div>');
+    var itemId = new Date().getTime();
+    $('#files').prepend('<div data-id="' + itemId + '" class="file">\n<div><strong>Filename</strong>: ' + this.value + ' <span><img style="margin:0;" src="/images/loading.gif" /></span></div>\n<div><strong>Size</strong>: </div>\n<blockquote></blockquote></div>');
     $('#item_id').val(itemId);
     $(this).closest('form').submit();
   });
 });
+
