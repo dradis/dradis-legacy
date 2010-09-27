bfaa8700a212e63c88a20467be4ca11a6702be41
Daniel Martin <etd(at)nomejortu.com>
2010-09-27 22:15:58 +0000

    Update these test cases to inherit from ActionController. This must be pretty old code which highlights we do not write enough tests :(

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -4,7 +4,7 @@ require 'sessions_controller'
 # Re-raise errors caught by the controller.
 class SessionsController; def rescue_action(e) raise e end; end
 
-class SessionsControllerTest < Test::Unit::TestCase
+class SessionsControllerTest < ActionController::TestCase
   fixtures :users
 
   def setup
