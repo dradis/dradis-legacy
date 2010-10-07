4363dc15a59349fda1cad275de986c60e7c0935f
Daniel Martin <etd(at)nomejortu.com>
2010-10-07 07:53:40 +0000

    Instead of using the external RestfulController and RESTOpenrations mixin, we are bringin all the functionality back to the NodesController for clarity.
    We are also making use of the new Rails 3 construct that lets us define the
    formats supported by the controller with @respond_to@ and then specify the object
    or collection we want to respond with using @respond_with@.
    
    The next step will be to remove the JsonController altogether and use 
    NodesController for all the JSON operations (from the tree view)

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,5 +1,53 @@
 # This controller exposes the REST operations required to manage the Node 
-# resource. See RestfulController for details of the implementation.
+# resource.
 class NodesController < RestfulController
-  rest_operations_for :node, :include => :notes
+  before_filter :login_required
+  before_filter :find_or_initialize_node, :except => [ :index ]
+
+  respond_to :json
+
+  # GET /nodes
+  def index
+    @nodes = Node.all
+    respond_with(@nodes)
+  end        
+
+  # POST /nodes
+  def create
+    if @node.save
+      flash[:notice] = 'Successfully created node.'
+    end
+    respond_with(@node)
+  end
+
+  # GET /nodes/<id>
+  def show
+    respond_with(@node)
+  end
+
+  # PUT /node/<id>
+  def update
+    if @node.update_attributes( params[:node] )
+      flash[:notice] = 'Successfully updated node.' 
+    end
+    respond_with(@node)
+  end
+
+  # DELETE /nodes/<id>
+  def destroy
+    @node.destroy
+    respond_with(@node)
+  end
+
+  protected
+    def find_or_initialize_node
+      if params[:node_id]
+        unless @node = Node.find_by_id(params[:node_id])
+          render_optional_error_file :not_found
+        end
+      else
+        @node = Node.new(params[:node])
+      end
+    end        
+
 end
