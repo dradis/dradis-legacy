e4b0de4155b22b21821b6dcd7bd5a377e2855496
Daniel Martin <etd(at)nomejortu.com>
2010-10-08 14:07:52 +0000

    Provide a name for this upload plugin

--- vendor/plugins/typhon_upload/lib/typhon_upload/meta.rb
+++ vendor/plugins/typhon_upload/lib/typhon_upload/meta.rb
@@ -1,6 +1,6 @@
 module TyphonUpload  
   module Meta
-    NAME = "This plugin does not have a name, define it at #{__FILE__.gsub(/#{RAILS_ROOT}/,'.')}"
+    NAME = "Typhon III vulnerability scanner output file upload"
     # change this to the appropriate version
     module VERSION #:nodoc:
       MAJOR = 2
