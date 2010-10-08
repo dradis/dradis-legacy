556bdf86dd50cd8ea7afd1826bee05080477ca4c
Daniel Martin <etd(at)nomejortu.com>
2010-10-08 13:35:47 +0000

    Fix a typo in the module name of this *upload* plugin

--- vendor/plugins/nessus_upload/lib/nessus_upload/meta.rb
+++ vendor/plugins/nessus_upload/lib/nessus_upload/meta.rb
@@ -1,4 +1,4 @@
-module NessusUploadImport  
+module NessusUpload
   module Meta
     NAME = "This plugin does not have a name, define it at #{__FILE__.gsub(/#{RAILS_ROOT}/,'.')}"
     # change this to the appropriate version
