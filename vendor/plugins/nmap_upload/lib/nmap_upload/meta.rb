735d16beaca4a66a4897fb463214bcacf5918028
Daniel Martin <etd(at)nomejortu.com>
2010-10-08 13:34:46 +0000

    Fix a typo in the module name of this *upload* plugin

--- vendor/plugins/nmap_upload/lib/nmap_upload/meta.rb
+++ vendor/plugins/nmap_upload/lib/nmap_upload/meta.rb
@@ -1,4 +1,4 @@
-module NmapUploadImport  
+module NmapUpload
   module Meta
     NAME = "This plugin does not have a name, define it at #{__FILE__.gsub(/#{RAILS_ROOT}/,'.')}"
     # change this to the appropriate version
