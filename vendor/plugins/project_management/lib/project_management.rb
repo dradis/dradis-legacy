ac4f4fa73e5f16cbc08d9e38687a20b219676daa
Daniel Martin <etd(at)nomejortu.com>
2010-10-08 13:33:23 +0000

    The Project Management plugin was missing the meta-information

--- vendor/plugins/project_management/lib/project_management.rb
+++ vendor/plugins/project_management/lib/project_management.rb
@@ -5,6 +5,7 @@ require 'project_management/export/meta_server_processor'
 require 'project_management/actions'
 require 'project_management/upload/template'
 require 'project_management/upload/package'
+require 'project_management/meta'
 
 module ProjectManagement
   CONF_FILE = File.join(RAILS_ROOT, 'config', 'project_management.yml')
---
+++ vendor/plugins/project_management/lib/project_management/meta.rb
@@ -0,0 +1,26 @@
+module ProjectPackageUpload 
+  module Meta
+    NAME = "Project package (.zip) upload"
+    # change this to the appropriate version
+    module VERSION #:nodoc:
+      MAJOR = 2
+      MINOR = 6
+      TINY = 0
+
+      STRING = [MAJOR, MINOR, TINY].join('.')
+    end
+  end
+end
+module ProjectTemplateUpload 
+  module Meta
+    NAME = "Project template (.xml) upload"
+    # change this to the appropriate version
+    module VERSION #:nodoc:
+      MAJOR = 2
+      MINOR = 6
+      TINY = 0
+
+      STRING = [MAJOR, MINOR, TINY].join('.')
+    end
+  end
+end
