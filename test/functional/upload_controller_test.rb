b2837e3c3a87a4b676cca9fcda1b6acca48eb935
Daniel Martin <etd(at)nomejortu.com>
2010-10-15 06:42:59 +0000

    At some point we need to make sure that whenever there is an exception in the upload plugin processing, the user is notified

--- test/functional/upload_controller_test.rb
+++ test/functional/upload_controller_test.rb
@@ -1,8 +1,23 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
+module Plugins::Upload
+  module DummyUpload
+    def self.import()
+      raise 'This upload always fails!'
+    end 
+  end
+end
+
 class UploadControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_processingexception
+    job_id = Time.now.to_i
+
+    # 1st start the background processing of the upload
+    Delayed::Job::enqueue( 'DummyUpload', '/tmp/nonexistent', job_id )
+    sleep 3
+
+    get(:status, {:after => 0, :item_id => job_id})
+
+    assert assigns[:logs].collect(&:text).includes?('This upload always fails!')
   end
 end
