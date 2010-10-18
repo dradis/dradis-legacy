7d3930c3f963beb0c56621c47db4598c68967fae
Daniel Martin <etd(at)nomejortu.com>
2010-10-18 22:14:39 +0000

    Simplify the ExportPluginGenerator and bring it in line with Rails 3 generators. See: http://dradisframework.org/community/index.php?topic=363.0
    Thanks to raesene for being the first to point out there was something wrong with
    Dradis generators.

--- lib/generators/export_plugin/export_plugin_generator.rb
+++ lib/generators/export_plugin/export_plugin_generator.rb
@@ -4,7 +4,7 @@
 # if this is not part of the plugin name argument.
 #
 # This creates a plugin in <tt>vendor/plugins/</tt> including an <tt>init.rb</tt> 
-# and +README+ as well as standard <tt>lib/</tt>, <tt>task/</tt>, and 
+# and +README+ as well as standard <tt>lib/</tt>, <tt>lib/tasks/</tt>, and 
 # <tt>test/</tt> directories.
 #
 # ==Note
@@ -24,41 +24,35 @@
 #       vendor/plugins/word_export/install.rb
 #       vendor/plugins/word_export/lib/word_export.rb
 #       vendor/plugins/word_export/test/word_export_test.rb
-#       vendor/plugins/word_export/tasks/word_export_tasks.rake
-class ExportPluginGenerator < Rails::Generator::NamedBase
-  attr_reader :plugin_path # :nodoc:
+#       vendor/plugins/word_export/lib/tasks/word_export_tasks.rake
+class ExportPluginGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
 
-  def initialize(runtime_args, runtime_options = {}) # :nodoc:
-    runtime_args[0] = runtime_args[0].underscore + "_export" if runtime_args[0] && !(runtime_args[0].tableize =~ /.*_export/)
+  # Check for collisions in generated class name
+  # TODO: not too sure about this
+  # check_class_collision :suffix => "Export"
+
+  def initialize(args, *options) # :nodoc:
+    args[0] = args[0].underscore + "_export" if args[0] && !(args[0].tableize =~ /.*_export/)
     super
-    @plugin_path = "vendor/plugins/#{file_name}"
+
+    #Set the destination root for the plugin
+    self.destination_root=( File.join( Rails.root, 'vendor', 'plugins') )
   end
 
-  def manifest # :nodoc:
-    record do |m|
-      # Check for class naming collisions.
-      m.class_collisions class_path, class_name
+  def generate_files # :nodoc:
+    template 'README',        "#{file_name}/README"
+    template 'MIT-LICENSE',   "#{file_name}/MIT-LICENSE"
+    template 'Rakefile',      "#{file_name}/Rakefile"
+    template 'init.rb',       "#{file_name}/init.rb"
+    template 'install.rb',    "#{file_name}/install.rb"
+    template 'uninstall.rb',  "#{file_name}/uninstall.rb"
+    template 'plugin.rb',     "#{file_name}/lib/#{file_name}.rb"
+    template 'tasks.rake',    "#{file_name}/lib/tasks/#{file_name}_tasks.rake"
+    template 'unit_test.rb',  "#{file_name}/test/#{file_name}_test.rb"
+    template 'actions.rb',    "#{file_name}/lib/#{file_name}/actions.rb"
+    template 'version.rb',    "#{file_name}/lib/#{file_name}/version.rb"
 
-      m.directory "#{plugin_path}/lib"
-      m.directory "#{plugin_path}/lib/#{file_name}"
-      m.directory "#{plugin_path}/tasks"
-      m.directory "#{plugin_path}/test"
-
-      m.template 'README',        "#{plugin_path}/README"
-      m.template 'MIT-LICENSE',   "#{plugin_path}/MIT-LICENSE"
-      m.template 'Rakefile',      "#{plugin_path}/Rakefile"
-      m.template 'init.rb',       "#{plugin_path}/init.rb"
-      m.template 'install.rb',    "#{plugin_path}/install.rb"
-      m.template 'uninstall.rb',  "#{plugin_path}/uninstall.rb"
-      m.template 'plugin.rb',     "#{plugin_path}/lib/#{file_name}.rb"
-      m.template 'tasks.rake',    "#{plugin_path}/tasks/#{file_name}_tasks.rake"
-      m.template 'unit_test.rb',  "#{plugin_path}/test/#{file_name}_test.rb"
-      m.template 'actions.rb',    "#{plugin_path}/lib/#{file_name}/actions.rb"
-      m.template 'version.rb',    "#{plugin_path}/lib/#{file_name}/version.rb"
-      m.template 'USAGE',         "#{plugin_path}/USAGE"
-
-      m.readme "USAGE"
-
-    end
+    readme "USAGE"
   end
 end
