da252b490998feb152e39eae1d5b8ed138d339ca
Daniel Martin <etd(at)nomejortu.com>
2010-10-27 14:44:03 +0000

    Simplify the ImportPluginGenerator and bring it in line with Rails 3 generators. See: http://dradisframework.org/community/index.php?topic=363.0

--- lib/generators/import_plugin/import_plugin_generator.rb
+++ lib/generators/import_plugin/import_plugin_generator.rb
@@ -24,42 +24,36 @@
 #       vendor/plugins/wiki_import/install.rb
 #       vendor/plugins/wiki_import/lib/wiki_import.rb
 #       vendor/plugins/wiki_import/test/wiki_import_test.rb
-#       vendor/plugins/wiki_import/tasks/wiki_import_tasks.rake
-class ImportPluginGenerator < Rails::Generator::NamedBase
-  attr_reader :plugin_path # :nodoc:
+#       vendor/plugins/wiki_import/lib/tasks/wiki_import_tasks.rake
+class ImportPluginGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
 
-  def initialize(runtime_args, runtime_options = {}) # :nodoc:
-    runtime_args[0] = runtime_args[0].underscore + "_import" if runtime_args[0] && !(runtime_args[0].tableize =~ /.*_import/)
+  def initialize(args, *options) # :nodoc:
+    args[0] = args[0].underscore + "_import" if args[0] && !(args[0].tableize =~ /.*_import/)
     super
-    @plugin_path = "vendor/plugins/#{file_name}"
+
+    #Set the destination root for the plugin
+    self.destination_root=( File.join( Rails.root, 'vendor', 'plugins') )
+
+    # Check for class naming collisions.
+    # This checks that no Ruby/Rails classes are overriden but not other app-layer
+    class_collisions class_name
   end
 
-  def manifest # :nodoc:
-    record do |m|
-      # Check for class naming collisions.
-      m.class_collisions class_path, class_name
+  def generate_files # :nodoc:
+    template 'README',        "#{file_name}/README"
+    template 'MIT-LICENSE',   "#{file_name}/MIT-LICENSE"
+    template 'Rakefile',      "#{file_name}/Rakefile"
+    template 'init.rb',       "#{file_name}/init.rb"
+    template 'install.rb',    "#{file_name}/install.rb"
+    template 'uninstall.rb',  "#{file_name}/uninstall.rb"
+    template 'config.yml',    "#{file_name}/config/#{file_name}.yml"
+    template 'plugin.rb',     "#{file_name}/lib/#{file_name}.rb"
+    template 'tasks.rake',    "#{file_name}/lib/tasks/#{file_name}_tasks.rake"
+    template 'unit_test.rb',  "#{file_name}/test/#{file_name}_test.rb"
+    template 'meta.rb',       "#{file_name}/lib/#{file_name}/meta.rb"
+    template 'filters.rb',    "#{file_name}/lib/#{file_name}/filters.rb"
 
-      m.directory "#{plugin_path}/config"
-      m.directory "#{plugin_path}/lib"
-      m.directory "#{plugin_path}/lib/#{file_name}"
-      m.directory "#{plugin_path}/tasks"
-      m.directory "#{plugin_path}/test"
-
-      m.template 'README',        "#{plugin_path}/README"
-      m.template 'MIT-LICENSE',   "#{plugin_path}/MIT-LICENSE"
-      m.template 'Rakefile',      "#{plugin_path}/Rakefile"
-      m.template 'init.rb',       "#{plugin_path}/init.rb"
-      m.template 'install.rb',    "#{plugin_path}/install.rb"
-      m.template 'uninstall.rb',  "#{plugin_path}/uninstall.rb"
-      m.template 'config.yml',  "#{plugin_path}/config/#{file_name}.yml"
-      m.template 'plugin.rb',     "#{plugin_path}/lib/#{file_name}.rb"
-      m.template 'tasks.rake',    "#{plugin_path}/tasks/#{file_name}_tasks.rake"
-      m.template 'unit_test.rb',  "#{plugin_path}/test/#{file_name}_test.rb"
-      m.template 'meta.rb',       "#{plugin_path}/lib/#{file_name}/meta.rb"
-      m.template 'filters.rb',    "#{plugin_path}/lib/#{file_name}/filters.rb"
-      m.template 'USAGE',         "#{plugin_path}/USAGE"
-
-      m.readme "USAGE"
-    end
+    readme "USAGE"
   end
 end
