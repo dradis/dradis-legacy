c632b08784e87ff07d3552aaf6155c87f4601ba5
Daniel Martin <etd(at)nomejortu.com>
2010-12-17 10:39:19 +0000

    Split the specs in two groups, anonymous/unauthenticated and authenticated.
    Make use of the macros defined in ControllerMacros to test the authentication filter.

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe AttachmentsController do
 
-  it "should save a file without extension"
+  describe "as guest" do
+    it_should_require_authentication
+  end
 
-  it "should show an attachment without extension"
+  describe "as user" do
+    before(:each) do
+      login_as_user
+    end
 
-  it "should auto-rename if file name already exists"
+    it "should pass params to the attachment object"
+
+    it "should save a file without extension"
+
+    it "should show an attachment without extension"
+
+    it "should auto-rename if file name already exists"
+  end
 end
