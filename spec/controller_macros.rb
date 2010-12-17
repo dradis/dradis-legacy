62da739906c8e232aed3992d5997f47344bfbcaa
Daniel Martin <etd(at)nomejortu.com>
2010-12-17 11:15:35 +0000

    Extend the @it_should_require_authentication@ method to cover all the actions defined in the controller. It will then create a spec for each action. It also accounts for the posibility of some actions not requiring authentication via the :except option.
    Each controller spec using this macro will need to pass the controller name as an argument. We should investigate if there is an auto-magic way of getting the current controller name though.

--- spec/controller_macros.rb
+++ spec/controller_macros.rb
@@ -5,14 +5,25 @@ module ControllerMacros
   end
 
   module ClassMethods
+    # Get all actions for specified controller
+    def get_all_actions(controller_name)
+      controller_class = Module.const_get(controller_name.to_s.pluralize.capitalize + "Controller")
+      controller_class.public_instance_methods(false).reject{ |action| ['rescue_action'].include?(action) || (action =~ /_one_time_conditions/) }
+    end
+
 
     # This method goes through the controller actions and creates a new spec to
     # ensure they all require authentication.
-    def it_should_require_authentication
-      it "should require authentication for :show" do
-        get :show, :id => 1
-        response.should redirect_to(login_path)
-        flash[:notice].should == 'Access denied.'
+    def it_should_require_authentication(controller, options={})
+      except= options[:except] || []
+      actions_to_test= get_all_actions(controller).reject{ |a| except.include?(a) }
+      actions_to_test += options[:include] if options[:include]
+      actions_to_test.each do |action|
+        it "should require authentication for :#{action}" do
+          get action 
+          response.should redirect_to(login_path)
+          flash[:notice].should == 'Access denied.'
+        end
       end
     end
   end
