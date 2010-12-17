86370dbac15a8042b858a8d0d21291e55759e64c
Daniel Martin <etd(at)nomejortu.com>
2010-12-17 08:10:53 +0000

    Lets start by testing the AttachmentsController code

---
+++ spec/controllers/attachments_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe AttachmentsController do
+
+  it "should save a file without extension"
+
+  it "should show an attachment without extension"
+
+  it "should auto-rename if file name already exists"
+end
