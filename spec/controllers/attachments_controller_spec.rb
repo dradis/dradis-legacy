302be863f4fcbb22a9cde36ad81b463f605a2a11
Daniel Martin <etd(at)nomejortu.com>
2010-12-18 20:56:33 +0000

    * Include the Configuration fixtures, otherwise the RevisionObserver will fail when we it tries to query the current revision. * Update the call to @it_should_require_authentication@ to include the controller name as required by the new implementation of the macro. * Specify two new cases: ** it should fail when no :node_id is passed ** it should fail if no file is uploaded

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 describe AttachmentsController do
+  fixtures :configurations
 
   describe "as guest" do
-    it_should_require_authentication
+    it_should_require_authentication :attachment
   end
 
   describe "as user" do
+
     before(:each) do
       login_as_user
     end
+
+    it "should fail if no Node id is passed" do
+      get :index
+      flash[:error].should_not be_nil
+    end
+
+    it "should fail if no file is uploaded"
 
     it "should pass params to the attachment object"
 
