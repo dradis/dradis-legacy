38872653cb66e2d4f9c819bfdb70709bd42fb76e
Daniel Martin <etd(at)nomejortu.com>
2011-02-21 14:58:28 +0000

    Move from the constant-based configuration to using the Configuration Manager to store settings for the HTMLExport plugin.

--- vendor/plugins/html_export/init.rb
+++ vendor/plugins/html_export/init.rb
@@ -1,4 +1,4 @@
 require 'html_export'
 
 
-Category.find_or_create_by_name( HTMLExport::REPORTING_CATEGORY_NAME )
+Category.find_or_create_by_name( HTMLExport::Configuration.category )
--- vendor/plugins/html_export/lib/html_export.rb
+++ vendor/plugins/html_export/lib/html_export.rb
@@ -4,10 +4,11 @@ require 'html_export/actions'
 require 'html_export/version'
 
 module HTMLExport
-  REPORTING_CATEGORY_NAME = 'HTMLExport ready'
-  CONF = {
-    :template => Rails.root.join( 'vendor', 'plugins', 'html_export', 'template.html.erb' )
-  }
+  class Configuration < Core::Configurator
+    configure :namespace => 'htmlexport'
+    setting :category, :default => 'HTMLExport ready'
+    setting :template, :default => Rails.root.join( 'vendor', 'plugins', 'html_export', 'template.html.erb' )
+  end
 end
 
 # This includes the export plugin module in the dradis export plugin repository
