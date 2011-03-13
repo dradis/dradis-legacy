bdaa0482ff0094f14215832cfffb4acbe9a6eee1
Daniel Martin <etd(at)nomejortu.com>
2011-03-13 19:49:19 +0000

    Add some configuration options for the BurpUpload. We start with the name of the category that the plugin will assign to the notes.

--- vendor/plugins/burp_upload/lib/burp_upload.rb
+++ vendor/plugins/burp_upload/lib/burp_upload.rb
@@ -4,6 +4,13 @@ require 'burp_upload/parser'
 require 'burp_upload/filters'
 require 'burp_upload/meta'
 
+module BurpUpload
+  class Configuration < Core::Configurator
+    configure :namespace => 'burp'
+    setting :category, :default => 'Burp Scanner output'
+  end
+end
+
 # This includes the import plugin module in the dradis import plugin repository
 module Plugins
   module Upload 
