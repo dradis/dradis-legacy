91ca9aa42e5d3cba2b1f0b832bb252a425129a4a
Daniel Martin <etd(at)nomejortu.com>
2011-04-12 11:32:07 +0000

    Make sure we include the CSRF token in this non-ExtJS component.
    Related to r2494 and r2505

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,6 +1,11 @@
-jQuery.ajaxSetup({
-  'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+jQuery.ajaxSetup({ 
+  beforeSend: function(xhr) { 
+    xhr.setRequestHeader("Accept", "text/javascript"); 
+    var token = $("meta[name='csrf-token']").attr("content");
+    xhr.setRequestHeader("X-CSRF-Token", token);
+  }
 });
+
 
 $(function(){
   $('form').ajaxForm({dataType: 'script'});
