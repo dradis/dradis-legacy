3f417f11f81703dbe326688597449d508e78aa1a
Daniel Martin <etd(at)nomejortu.com>
2011-04-28 14:15:02 +0000

    Implement the first spec for the AttachmentController to upload a simple file.
    There are two gotchas:
    * :before and :after every spec we have to create and destroy the corresponding Node
    * we need a dummy upload file that we've implemented through ActionDispatch::Http::UploadedFile

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe AttachmentsController do
-  fixtures :configurations
+
+  def mock_uploader(file, type = 'image/png')
+    uploader = ActionDispatch::Http::UploadedFile.new({:filename => file.path, :type => type, :tempfile => file })
+    def uploader.read
+      File.read(path)
+    end
+    def uploader.size
+      File.stat(path).size
+    end
+    uploader
+  end
+
 
   describe "as guest" do
     it_should_require_authentication :attachment
@@ -11,9 +22,23 @@ describe AttachmentsController do
 
     before(:each) do
       login_as_user
+      @node = Factory.create(:node)
+    end
+    after(:each) do
+      @node.destroy
     end
 
     it_should_require_parent_resource_id(:attachment)
+
+    it "should save an uploaded file as an attachment" do
+      upload = mock_uploader File.new('public/images/rails.png')
+
+      post :create, :attachment_file => upload, :node_id => @node.id
+
+      response.should be_success
+      @node.attachments.should have(1).item
+      @node.attachments.first.filename.should == 'rails.png'
+    end
 
     it "should fail if no file is uploaded"
 
