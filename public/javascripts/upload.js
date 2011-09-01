abf7e97d8d847339a5bfe42eab80745e793de412
Daniel Martin <etd-github@nomejortu.com>
2011-09-01 14:19:01 -0700

    Convert the relative path into an absolute path
    When Dradis runs under Apache on a subdirectory (e.g. /dradis/) this asset
    won't be found. We have to change it to a relative path.
    
    This commit closes #37

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -13,7 +13,7 @@ $(function(){
   $(':file').change(function(){
     $('#files').empty();
     var itemId = new Date().getTime();
-    $('#files').prepend('<div data-id="' + itemId + '" class="file">\n<div><strong>Filename</strong>: ' + this.value + ' <span><img style="margin:0;" src="/images/loading.gif" /></span></div>\n<div><strong>Size</strong>: </div>\n<div id="console"></div></div>');
+    $('#files').prepend('<div data-id="' + itemId + '" class="file">\n<div><strong>Filename</strong>: ' + this.value + ' <span><img style="margin:0;" src="images/loading.gif" /></span></div>\n<div><strong>Size</strong>: </div>\n<div id="console"></div></div>');
     $('#item_id').val(itemId);
     $(this).closest('form').submit();
   });
