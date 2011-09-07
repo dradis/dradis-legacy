1e98f32453a90b71db45ac1708691eff190ab163
Daniel Martin <etd-github@nomejortu.com>
2011-09-07 10:15:20 +0100

    Move the ControllerMacros to rspec's support dir
    This is loaded by default so we can cleanup the spec_helper.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require File.expand_path("../controller_macros", __FILE__)
 require File.expand_path("../factories", __FILE__)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
