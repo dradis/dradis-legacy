3c4e1516f12dcf5716623633140d7c2369229208
Daniel Martin <etd-github@nomejortu.com>
2011-09-20 19:44:53 +0200

    New LogsController to access the audit log
    This controller is used by the Ajax poller to retrieve changes made by other
    users.
    
    Only Logs#index.json is defined. It returns all audit logs (:uid => 0) that are
    associated with *other* users. This is because the current user's interface was
    already updated in the browser before the actions reached the server and there
    is no need for the interface to parse them again.

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,21 @@
+# This controller is used by the Ajax poller to retrieve changes made by other
+# users.
+
+class LogsController < AuthenticatedController
+  respond_to :json
+
+  # Returns an array of log entries in the form of:
+  #  { :id => log.id, :action => 'create|update|destroy', :record => <attributes> }
+  def index
+    after = params.fetch(:after, 0).to_i
+    @logs = Log.where('uid = 0 and id > ?', after).collect do |log_entry|
+      event_data = YAML.load(log_entry.text)
+
+      # Avoid re-sending our own events, the interface already has those
+      next if event_data[:by] == current_user
+      event_data.merge(:id => log_entry.id)
+    end
+
+    respond_with(@logs.compact)
+  end
+end
