fcf2e67bb598d957949e5b98f916c3063e377a79
Daniel Martin <etd-github@nomejortu.com>
2011-09-20 23:00:13 +0200

    New Ajax *smart* polling
    The statusUpdate() function supersedes the checkrevision() function and provides
    a smarter approach to polling for updates from the server:
    
    * A request to /logs.json?after=<id> is made every ten seconds by the Ext.TaskMgr
    
    * The server returns a list of all the updates made by other users to the
    repository
    
    * Each update is considered and the corresponding widgets are notified if they
    have to be updated.
    
    * The id of the las audit entry received is saved so it can be used in the next
    round of polling.

--- public/javascripts/thebrain.js
+++ public/javascripts/thebrain.js
@@ -36,3 +36,50 @@ function checkrevision(interval) {
   })
 
 }
+
+function statusUpdate(lastUpdate){
+  console.log(this);
+  Ext.Ajax.request({
+    url: 'logs.json',
+    method: 'GET',
+    params: {after: this.after},
+    scope: this, // make sure the task config is available in the callsbacks
+    success: function(response, options){
+      var updates = Ext.decode(response.responseText);
+
+      Ext.each(updates, function(update) {
+        // Category-related events are not important as changing to a new node
+        // will refresh the list of categories anyway
+        if (update.resource == 'category') {
+          console.log('skipping category event');
+          return;
+        }
+
+        if (update.resource == 'note') {
+          // If there is a note-related event but for a different node we are not
+          // interested
+          if (update.record.node_id != this.current_node) {
+            console.log('skipping note update for different node');
+            return;
+          }
+          console.log('marking notes browser view as dirty');
+          notesbrowser.markDirty();
+        }
+
+        // Skip node events for the time being
+        if (update.resource == 'node') {
+          console.log('skipping node event');
+          return;
+        }
+      }, this);
+
+      // from now on, only request updates after the last one we just received
+      if (updates.length > 0) {
+        this.after = updates[updates.length - 1].id;
+      }
+    },
+    failure: function(response, options){
+      dradisstatus.setStatus({text: 'An error occured while loading new updates', iconCls: 'error', clear: 5000});
+    }
+  });
+}
