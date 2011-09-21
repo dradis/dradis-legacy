361ed6f710cac74cb8bfa660645843bd75587c61
Daniel Martin <etd-github@nomejortu.com>
2011-09-21 17:27:33 +0200

    Include the EXPECTS constant and update version

--- vendor/plugins/nessus_upload/lib/nessus_upload/meta.rb
+++ vendor/plugins/nessus_upload/lib/nessus_upload/meta.rb
@@ -1,10 +1,11 @@
 module NessusUpload
   module Meta
-    NAME = "Nessus output (.xml) file upload"
+    NAME = "Nessus output (.nessus) file upload"
+    EXPECTS = "Nessus XML (V2) format."
     # change this to the appropriate version
     module VERSION #:nodoc:
       MAJOR = 2
-      MINOR = 7 
+      MINOR = 8
       TINY = 0
 
       STRING = [MAJOR, MINOR, TINY].join('.')
