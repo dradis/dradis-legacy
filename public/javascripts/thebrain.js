21b664858b66cb881b7bd76f811abd4e66a5e3d3
Daniel Martin <etd-github@nomejortu.com>
2011-09-22 14:12:00 +0200

    Remove the old revision poller. Clean up new func.
    * We are no longer using the checkrevision() function. It has been superseded
    by the new statusUpdate()
    * We're going to handle the initial delay outside this function so there is no
    need to check lastUpdate to ignore the first few runs.

--- public/javascripts/thebrain.js
+++ public/javascripts/thebrain.js
@@ -1,47 +1,7 @@
 // TODO: this should be an object, not functions
 
 //------------------------------------------------------------------------ revision poller
-
-function checkrevision(interval) {
-  // prevent further requests
-  // this may be better done with Ext's TaskRunner
-  if (dradis.revision == -1) { return; }
-
-  Ext.Ajax.request({
-    url: 'configurations/1.xml',
-    method: 'get',
-    success: function(response, options) {
-              var msg = response.responseText;
-              // how ugly is this?
-              rev = msg.match(/<value>(.*)<\/value>/);
-              if (dradis.revision != eval(rev[1])) {
-                dradisstatus.setStatus({ 
-                  text: 'There is a new revision in the server. Please refresh.'
-                });
-
-                // prevent further requests
-                // this may be better done with Ext's TaskRunner
-                dradis.revision = -1;
-
-                Ext.TaskMgr.stopAll();
-              }
-    },
-    failure: function(response, options) {
-              dradisstatus.setStatus({
-                text: 'An error occured with the Ajax request',
-                iconCls: 'error',
-                clear: 5000
-              });
-    }
-  })
-
-}
-
 function statusUpdate(lastUpdate){
-  if (lastUpdate < 3) {
-    // wait at least 30 secs before polling
-    return;
-  }
   Ext.Ajax.request({
     url: 'logs.json',
     method: 'GET',
