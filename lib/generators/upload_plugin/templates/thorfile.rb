2e1b3541cdce3bee7fa5cb3ab55af70fb1a1556b
Daniel Martin <etd-github@nomejortu.com>
2011-10-28 14:57:40 +0200

    UploadPlugin:Generate a much more useful Thor task
    Instead of a bare bones Thor task, the generator now does file validation,
    creation of logger and invocation of the Plugin's import() method.

--- lib/generators/upload_plugin/templates/thorfile.rb
+++ lib/generators/upload_plugin/templates/thorfile.rb
@@ -2,11 +2,24 @@ class DradisTasks < Thor
   class Upload < Thor
     namespace "dradis:upload"
 
-#    desc  "<%= class_name.underscore.gsub(/_upload/, '') %>", "upload <%= class_name.underscore.gsub(/_upload/, '') %> results"
+#    desc  "<%= class_name.underscore.gsub(/_upload/, '') %> FILE", "upload <%= class_name.underscore.gsub(/_upload/, '') %> results"
 #    long_desc "This will appear if the user runs 'thor help dradis:upload:<%= class_name.underscore.gsub(/_upload/, '') %>'"
 #    def <%= class_name.underscore.gsub(/_upload/, '') %>(file_path)
 #      require 'config/environment'
 #
+#      logger = Logger.new(STDOUT)
+#      logger.level = Logger::DEBUG
+#
+#      unless File.exists?(file_path)
+#        $stderr.puts "** the file [#{file_path}] does not exist"
+#        exit -1
+#      end
+#
+#      <%= class_name %>.import(
+#        :file => file_path,
+#        :logger => logger)
+#
+#      logger.close
 #    end
 
   end
