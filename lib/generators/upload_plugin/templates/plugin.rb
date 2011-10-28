4a069fa06a0f3f48360eaca254f9aac4dde72775
Daniel Martin <etd-github@nomejortu.com>
2011-10-28 13:24:09 +0200

    This is the Upload Plugin genetor. s/import/upload/

--- lib/generators/upload_plugin/templates/plugin.rb
+++ lib/generators/upload_plugin/templates/plugin.rb
@@ -12,7 +12,7 @@ module <%= class_name %>
   end
 end
 
-# This includes the import plugin module in the dradis import plugin repository
+# This includes the upload plugin module in the Dradis upload plugin repository
 module Plugins
   module Upload 
     include <%= class_name %>
