16287a65b26a9f8594b01cd7346afed7c281ef76
Daniel Martin <etd-github@nomejortu.com>
2011-11-11 09:49:52 +0100

    Update the NessusUpload plugin to use Nokogiri
    Instead of using the old REXML-based Nessus::Parser switch to a Nokogiri-based
    approach.
    
    In order to carry on using the custom ERB templates for hosts and report items
    we need to make use of the Nessus::Host and Nessus::ReportItem helper classes
    that hide the complexity of the XML format behind a friendly Ruby interface.

--- vendor/plugins/nessus_upload/lib/nessus_upload/filters.rb
+++ vendor/plugins/nessus_upload/lib/nessus_upload/filters.rb
@@ -1,4 +1,5 @@
-require 'nessus/parser'
+require 'nessus/host'
+require 'nessus/report_item'
 
 module NessusUpload  
   @@logger = nil
@@ -13,7 +14,7 @@ module NessusUpload
     @@logger        = params.fetch(:logger, Rails.logger)
 
     @@logger.info{'Parsing nessus output file...'}
-    parser = Nessus::Parser.parse_string(file_content)
+    doc = Nokogiri::XML( file_content )
     @@logger.info{'Parsing done'}
 
     # Read the template file which are needed to create the note elements
@@ -22,11 +23,13 @@ module NessusUpload
 
     # get the "Nessus Output" category instance or create it if it does not exist
     category = Category.find_or_create_by_name( NessusUpload::Configuration.category )
+    # every note we create will be assigned to this author
+    author = Configuration.author
     # create the parent early so we can use it to provide feedback on errors
     parent = Node.find_or_create_by_label( Configuration.parent_node)
 
-    if parser.reports.empty?
-      error = "No reports were detected in the uploaded file (//NessusClientData_v2/Report). Ensure you uploaded a Nessus XML v2 (.nessus) report."
+    if doc.xpath('/NessusClientData_v2/Report').empty?
+      error = "No reports were detected in the uploaded file (/NessusClientData_v2/Report). Ensure you uploaded a Nessus XML v2 (.nessus) report."
       @@logger.fatal{ error }
       parent.notes.create(
         :author => Configuration.author,
@@ -35,45 +38,55 @@ module NessusUpload
       return false
     end
 
-    parser.reports.each do |report|
-      @@logger.info{ "Processing report: #{report.name}" }
-      report_label = report.name
+    doc.xpath('/NessusClientData_v2/Report').each do |xml_report|
+      report_label = xml_report.attributes['name'].value
+      @@logger.info{ "Processing report: #{report_label}" }
       report_node = parent.children.find_or_create_by_label(report_label)
       
-      report.hosts.each do |host|
-        host_label = host.name
-        host_label = "#{host_label} (#{host.fqdn})" if host.fqdn
+      xml_report.xpath('./ReportHost').each do |xml_host|
+        host_label = xml_host.attributes['name'].value
+        host_label += " (#{xml_host.attributes['fqdn'].value})" if xml_host.attributes['fqdn']
+
         host_node = report_node.children.find_or_create_by_label_and_type_id(host_label, Node::Types::HOST)
         @@logger.info{ "\tHost: #{host_label}" }
-          
+
+        # Nessus::Hosts translates complex XML format into a simple Ruby object
+        # that can be used inside ERB
+        host = Nessus::Host.new(xml_host)
         note_template   = ERB.new(host_item_template,0,'>')
         node_text       = note_template.result(binding)
         Note.create(
           :node_id     => host_node.id,
-          :author      => Configuration.author,
+          :author      => author,
           :category_id => category.id,
           :text        => node_text
         )
       
-        host.report_items.each do |report_item|
-          next if report_item.plugin_id == "0"
-          item_label = "#{report_item.port}/#{report_item.protocol}"
+        xml_host.xpath('./ReportItem').each do |xml_report_item|
+          next if xml_report_item.attributes['pluginID'].value == "0"
+
+          item_label = xml_report_item.attributes['port'].value
+          item_label += "/"
+          item_label += xml_report_item.attributes['protocol'].value
           item_node  = host_node.children.find_or_create_by_label(item_label)
-      
+
+          # Nessus::ReportItem translate complex XML format into a simple Ruby
+          # object that can be used inside ERB
+          report_item = Nessus::ReportItem.new(xml_report_item)
           note_template   = ERB.new(report_item_template,0,'>')
           node_text       = note_template.result(binding)
           Note.create(
             :node_id     => item_node.id,
-            :author      => Configuration.author, 
+            :author      => author,
             :category_id => category.id,
             :text        => node_text 
           )
-        end
+        end #/ReportItem
 
-      end #/report
+      end #/ReportHost
       @@logger.info{ "Report processed." }
 
-    end	#/parser
+    end	#/Report
     
     return true
   end
