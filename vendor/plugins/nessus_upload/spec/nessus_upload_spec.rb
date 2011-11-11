de118189e57546141ddb4e2dc2c065e840d9ab5f
Daniel Martin <etd-github@nomejortu.com>
2011-11-11 17:36:18 +0100

    NessusUpload: Update spec with 1 new assertion and 1 pending spec

--- vendor/plugins/nessus_upload/spec/nessus_upload_spec.rb
+++ vendor/plugins/nessus_upload/spec/nessus_upload_spec.rb
@@ -28,9 +28,21 @@ end
 describe 'NessusUpload plugin' do
   include NessusUploadSpecHelper
 
+  # These are the properties we need to support:
+  # host.name                 The name given at scan time, usually an IP address
+  # host.ip                   The ip address of the host
+  # host.fqdn                 The full qualified domain name of the host
+  # host.operating_system     The OS of the system if detected
+  # host.mac_address          The mac address if the scanned system was on the same subnet
+  # host.netbios_name         The netbios name of the system
+  # host.scan_start_time      The date/time the scan started
+  # host.scan_stop_time       The date/time the scan ended
   it 'Nessus::Host responds to all the expected fields' do
     doc = Nokogiri::XML( XML1 )
     host = Nessus::Host.new( doc.xpath('/NessusClientData_v2/Report/ReportHost').first )
     host.name.should eq('10.0.0.1')
+    host.scan_start_time eq('Tue Aug 9 09:59:24 2011')
   end
+
+  pending 'Nessus::Host should provide access to each of its ReportItems'
 end
