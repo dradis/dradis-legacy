d3f52abf3b04b23e2bf6351a9ae8be47a1df13dd
Daniel Martin <etd-github@nomejortu.com>
2011-12-28 19:34:26 +0100

    Update Rails config files to 3.1 defaults
    See:
      http://railscasts.com/episodes/282-upgrading-to-rails-3-1

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-#ENV['RAILS_ENV'] = 'production'
-#RAILS_ENV.replace('production') if defined?(RAILS_ENV)
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
-require 'rails/all'
-
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Dradis
   class Application < Rails::Application
@@ -59,6 +65,12 @@ module Dradis
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
 
