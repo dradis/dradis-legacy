1e4b54c40a54d3769d67ccd6b5193ed14d34c72b
Daniel Martin <etd-github@nomejortu.com>
2012-01-21 18:21:13 +0100

    NiktoUpload: replace some tabs with spaces

--- vendor/plugins/nikto_upload/lib/nikto_upload.rb
+++ vendor/plugins/nikto_upload/lib/nikto_upload.rb
@@ -7,17 +7,14 @@ module NiktoUpload
   class Configuration < Core::Configurator
     configure :namespace => 'nikto_upload'
 
-	# name of the note cateory that is created
-	setting :category, :default => "Nikto output"
-
-	# name of the author
-	setting :author, :default => "Nikto plugin"
-
-	# name of the node that will be created in your repository tree that will be ancestor to all plugin-generated content
-	setting :parent_node, :default => "Nikto scan"
-
-    # setting :my_setting, :default => 'Something'
-    # setting :another, :default => 'Something Else'
+    # name of the note cateory that is created
+    setting :category, :default => "Nikto output"
+    
+    # name of the author
+    setting :author, :default => "Nikto plugin"
+    
+    # name of the node that will be created in your repository tree that will be ancestor to all plugin-generated content
+    setting :parent_node, :default => "Nikto scan"
   end
 end
 
