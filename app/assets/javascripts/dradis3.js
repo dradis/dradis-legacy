b6838a7665c108739dcccdbbc3903825dcf728db
Daniel Martin <etd-github@nomejortu.com>
2012-01-27 19:23:52 +0100

    Cleanup Upload#index
    Using the content_for trick in the view doesn't work with the asset pipeline.
    We have to include upload.js in the dradis3.js bundle. Some of its code is
    the same we have in configurations.js so we can remove it.

--- app/assets/javascripts/dradis3.js
+++ app/assets/javascripts/dradis3.js
@@ -2,3 +2,4 @@
 //= require jquery_ujs
 //= require jquery.form
 //= require configurations
+//= require upload
--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -1,12 +1,3 @@
-jQuery.ajaxSetup({ 
-  beforeSend: function(xhr) { 
-    xhr.setRequestHeader("Accept", "text/javascript"); 
-    var token = $("meta[name='csrf-token']").attr("content");
-    xhr.setRequestHeader("X-CSRF-Token", token);
-  }
-});
-
-
 $(function(){
   $('form').ajaxForm({dataType: 'script'});
 
