05ad0e06a6b4d5c8ac0e9460d569543775766fa7
Daniel Martin <etd-github@nomejortu.com>
2012-01-28 12:07:13 +0100

    Banner layout: Clean up
    * Include banner.css into the asset pipeline
    * Include the controller_name into the <body>, this will enable us to in the
    future scope the CSS.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Dradis::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( dradis3.js dradis3.css )
+  config.assets.precompile += %w( banner.css dradis3.js dradis3.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
