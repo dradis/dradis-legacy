a2e3374d6bc1093e544bae3958768c9562c66fde
Daniel Martin <etd-github@nomejortu.com>
2012-01-31 18:31:42 +0100

    Remove unnecessary mime type definitions
    These are already defined in
      ./actionpack-3.2.0/lib/action_dispatch/http/mime_types.rb

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,7 +3,3 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-
-Mime::Type.register 'image/jpg', :jpg
-Mime::Type.register 'image/gif', :gif
-Mime::Type.register 'image/png', :png
