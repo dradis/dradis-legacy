33ecc8d31e6858270700c204896c74b5f0fbf18a
Daniel Martin <etd@nomejortu.com>
2012-07-10 21:13:21 +0200

    SpecHelper: fix typo

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -1,11 +1,12 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# Configure Rails Envinronment
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
+
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb")].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
