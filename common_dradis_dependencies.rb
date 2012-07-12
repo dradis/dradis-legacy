18958521b1cebfed5e3955aa816895dc033332d1
Daniel Martin <etd@nomejortu.com>
2012-07-12 18:58:02 +0200

    Put dependencies common to all gems in a separate file
    The idea is that individual gems will use this file to specify their
    dependencies using Bundler. For instance:
    
    https://github.com/dradis/dradisframework/blob/dradis3.x/core/Gemfile

---
+++ common_dradis_dependencies.rb
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'guard'
+  gem 'guard-rspec', '~> 0.5.0'
+  gem 'rspec-rails', '~> 2.9.0'
+end
+
+gemspec
