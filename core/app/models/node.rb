f05d25777a97e72311aa06c0a8728cb66ce32382
Daniel Martin <etd-github@nomejortu.com>
2012-08-11 13:14:21 +0200

    Node - Use Ruby 1.9 style hashes

--- core/app/models/node.rb
+++ core/app/models/node.rb
@@ -4,9 +4,9 @@ class Node < ActiveRecord::Base
 
   attr_accessible :label, :parent_id, :position, :type_id
 
-  validates :label, :presence => true
+  validates :label, presence: true
 
-  has_many :notes, :dependent => :destroy
+  has_many :notes, dependent: :destroy
   # has_many :attachments, :dependent => :destroy
 
   before_save {|record| record.position = 0 unless record.position }
