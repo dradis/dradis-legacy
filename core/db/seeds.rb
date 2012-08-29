7d287a046e917da9236866abda629f2493c80a8c
Daniel Martin <etd-github@nomejortu.com>
2012-08-30 00:13:56 +0200

    Remove rspec, thor, rails config, schema, seeds, etc. from the main repo

---
+++ core/db/seeds.rb
@@ -0,0 +1,6 @@
+Category.create(:name=>'default category')
+
+Configuration.create(:name=>'revision', :value=>'0')
+Configuration.create(:name=>'password', :value=>'improvable_dradis')
+Configuration.create(:name=>'uploads_node', :value=>'Uploaded files')
+Configuration.create(:name=>'emails_node', :value=>'Emailed notes')
