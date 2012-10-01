c2fc1341e2fd29683e061efa76724258eeacbdcd
Daniel Martin <etd-github@nomejortu.com>
2012-10-01 17:06:52 +0200

    Core - Scope models under Dradis:: namespace

---
+++ core/app/models/dradis/node.rb
@@ -0,0 +1,21 @@
+module Dradis
+  class Node < ActiveRecord::Base
+    include ActsAsTree
+
+    attr_accessible :label, :parent_id, :position, :type_id
+
+    validates :label, presence: true
+
+    has_many :notes, dependent: :destroy
+    # has_many :attachments, :dependent => :destroy
+
+    before_save {|record| record.position = 0 unless record.position }
+
+    acts_as_tree order: :label
+
+    module Types
+      DEFAULT = 0
+      HOST = 1
+    end
+  end
+end
