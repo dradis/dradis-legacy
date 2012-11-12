486ba753d05e250182d44b9411724ecf78205947
Daniel Martin <etd-github@nomejortu.com>
2012-11-12 16:11:39 +0100

    Fix the seeds.rb to use namespaced model names

--- core/lib/generators/dradis/install/templates/db/seeds.rb
+++ core/lib/generators/dradis/install/templates/db/seeds.rb
@@ -1,6 +1,6 @@
-Category.create(:name=>'default category')
+Dradis::Category.create(:name=>'default category')
 
-Configuration.create(:name=>'revision', :value=>'0')
-Configuration.create(:name=>'password', :value=>'improvable_dradis')
-Configuration.create(:name=>'uploads_node', :value=>'Uploaded files')
-Configuration.create(:name=>'emails_node', :value=>'Emailed notes')
+Dradis::Configuration.create(:name=>'revision', :value=>'0')
+Dradis::Configuration.create(:name=>'password', :value=>'improvable_dradis')
+Dradis::Configuration.create(:name=>'uploads_node', :value=>'Uploaded files')
+Dradis::Configuration.create(:name=>'emails_node', :value=>'Emailed notes')
