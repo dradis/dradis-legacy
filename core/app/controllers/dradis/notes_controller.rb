50cf0627a3422d2631a7a00929543caf4ee9a769
Daniel Martin <etd-github@nomejortu.com>
2012-11-13 17:30:17 +0100

    Implement Notes#index (:json)

--- core/app/controllers/dradis/notes_controller.rb
+++ core/app/controllers/dradis/notes_controller.rb
@@ -2,10 +2,41 @@ module Dradis
   class NotesController < AuthenticatedController
     before_filter :find_or_initialize_node
     before_filter :find_or_initialize_note, :except => [ :index ]
-    private
+
+    # Retrieve the list of Note objects associated with a given Node.
+    # Formats supported: :json
+    def index
+      @notes = @node.notes
+      respond_to do |format|
+        format.json {
+          render :json => { :success => true,
+                            :data => @notes
+                          }
+        }
+      end
+    end
+
+    def new
+    end
+
+    def create
+      redirect_to root_path, notice: @note.save
+    end
 
     # TODO - implement CRUD actions
+    def edit
+      @nodes = Node.all
+    end
 
+    def update
+      if @note.update_attributes(params[:note])
+        redirect_to node_path(@node)
+      else
+        render :edit
+      end
+    end
+
+    private
     # For most of the operations of this controller we need to identify the Node
     # we are working with. This filter sets the @node instance variable if the 
     # give :node_id is valid.
@@ -29,7 +60,8 @@ module Dradis
         @note = Note.new(params[:note] || ActiveSupport::JSON.decode(params[:data]))
         @note.node = @node
       end
-      @note.updated_by = current_user
+      # TODO
+      # @note.updated_by = current_user
     end    
   end
 end
