543d886a3f933d39ff5637fe6d809dd957b1f741
Daniel Martin <etd-github@nomejortu.com>
2013-03-13 12:34:03 +0100

    Core - for the time being stick to the 'extjs' layout from the Frontend gem

--- core/app/controllers/dradis/authenticated_controller.rb
+++ core/app/controllers/dradis/authenticated_controller.rb
@@ -1,6 +1,8 @@
 module Dradis
   class AuthenticatedController < BaseController
     before_filter :login_required
+    # FIXME: this layout is in a different gem!
     layout 'dradis/extjs'
+    # layout 'dradis/preauth'
   end
 end
