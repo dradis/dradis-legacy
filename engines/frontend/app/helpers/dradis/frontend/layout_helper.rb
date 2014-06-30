813cb163602a490e2cc636608e6436cd9ed58ef0
Daniel Martin <etd-github@nomejortu.com>
2014-06-30 12:32:04 +0200

    Scope LayoutHelper under Dradis::Frontend

--- engines/frontend/app/helpers/dradis/frontend/layout_helper.rb
+++ engines/frontend/app/helpers/dradis/frontend/layout_helper.rb
@@ -2,21 +2,25 @@
 # This module should be included in all views globally,
 # to do so you may need to add this line to your ApplicationController
 #   helper :layout
-module LayoutHelper
-  def title(page_title, show_title = true)
-    content_for(:title) { h(page_title.to_s) }
-    @show_title = show_title
-  end
+module Dradis
+  module Frontend
+    module LayoutHelper
+      def title(page_title, show_title = true)
+        content_for(:title) { h(page_title.to_s) }
+        @show_title = show_title
+      end
 
-  def show_title?
-    @show_title
-  end
+      def show_title?
+        @show_title
+      end
 
-  def stylesheet(*args)
-    content_for(:head) { stylesheet_link_tag(*args) }
-  end
+      def stylesheet(*args)
+        content_for(:head) { stylesheet_link_tag(*args) }
+      end
 
-  def javascript(*args)
-    content_for(:head) { javascript_include_tag(*args) }
+      def javascript(*args)
+        content_for(:head) { javascript_include_tag(*args) }
+      end
+    end
   end
 end
