a3b0ba895179c1f98e61b6e479115e7a03ad1d7e
Daniel Martin <etd-github@nomejortu.com>
2014-07-07 16:50:03 +0200

    Bump to Rails 4.1.4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,9 +29,6 @@ class ActiveSupport::TestCase
   self.use_instantiated_fixtures  = false
 
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
