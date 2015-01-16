dradis/dradis-legacy

commit c722fb8706cc29bafae23cb420ad8603070e6f54

Issues#destroy - avoid ToCToR condition
We first pull the note IDs for the issues and then load them as Issue
object. An Ajax call to #destroy could alter the collection between the
time of check and the time of read causing us to read an ID that is no
longer valid.

engines/frontend/app/controllers/dradis/frontend/issues_controller.rb
@@ -80,8 +80,11 @@ module Dradis
       private
       def find_issues
         # FIXME: this hard-coding of the table name is problematic, it would be better to use Note.table_name
-        # @issues = Dradis::Core::Issue.find( Dradis::Core::Note.where(node_id: @issuelib).pluck('`notes`.`id`')).sort
-        @issues = Dradis::Core::Issue.find( Dradis::Core::Node.issue_library.notes.pluck('`dradis_notes`.`id`') ).sort
+        # We need a transaction because multiple DELETE calls can be issued from
+        # index and a TOCTOR can appear between the Note read and the Issue.find
+        Note.transaction do
+          @issues = Dradis::Core::Issue.find( Dradis::Core::Node.issue_library.notes.pluck('`dradis_notes`.`id`') ).sort
+        end
 
         @nodes = Dradis::Core::Node.includes(:children).all
         @new_node = Dradis::Core::Node.new

commit 212a1e0e6a007dba89f56e169fbca5c14e3b05e2

Issues#destroy - use the right class namespace for Note
This fixes a bug introduced in c722fb8706cc29bafae23cb420ad8603070e6f54
where we didn't reference the ::Dradis::Core namespace when working with
the Note class.

engines/frontend/app/controllers/dradis/frontend/issues_controller.rb
@@ -82,7 +82,7 @@ module Dradis
         # FIXME: this hard-coding of the table name is problematic, it would be better to use Note.table_name
         # We need a transaction because multiple DELETE calls can be issued from
         # index and a TOCTOR can appear between the Note read and the Issue.find
-        Note.transaction do
+        ::Dradis::Core::Note.transaction do
           @issues = Dradis::Core::Issue.find( Dradis::Core::Node.issue_library.notes.pluck('`dradis_notes`.`id`') ).sort
         end
 