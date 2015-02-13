305ab253385c352584ac9951b78dfce12c43d7c7
Simon Whittaker <simon@swbh.net>
2015-02-13 10:04:54 +0000

    Resolving issue with Actionview template error assets ActionView::Template::Error (Asset filtered out and will not be served

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( banner.css dradis3.js dradis3.css )
+Rails.application.config.assets.precompile += %w( banner.css dradis3.js dradis3.css OpenSans-Regular.eot OpenSans-Regular.woff OpenSans-Regular.ttf OpenSans-Regular.svg jquery.textile.loading.gif )
